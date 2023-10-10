31e7d7b81301b566e6c7bbb25ff643d1d3ac29f0
iraola <eduiraola@gmail.com>
2023-10-10 12:00:32 +0200

    Enhance comments

--- src/run.py
+++ src/run.py
@@ -9,22 +9,22 @@ import numpy as np
 @task(returns=1)
 def run():
     """
-        In this method we work with dimensions (main axes), which represent a
-        list of variables. For example, the value of each variable of a concrete
-        dimension could represent the power supplied by a generator, while the
-        value linked to that dimension should be the total sum of energy produced.
+    In this method we work with dimensions (main axes), which represent a
+    list of variables. For example, the value of each variable of a concrete
+    dimension could represent the power supplied by a generator, while the
+    value linked to that dimension should be the total sum of energy produced.
 
-        For each dimension it must be declared:
-            -variables: list of variables represented by tuples containing its
-                    lower and upper borders.
-            -n_cases: number of cases taken for each sample (each sample represents
-                    the total sum of a dimension). A case is a combination of
-                    variables where all summed together equals the sample.
-            -divs: number of divisions in that dimension. It will be the growth
-                    order of the number of cells
-            -lower: lower bound of the dimension (minimum value of a sample)
-            -upper: upper bound of the dimension (maximum value of a sample)
-            -label: dimension identifier
+    For each dimension it must be declared:
+        -variables: list of variables represented by tuples containing its
+                lower and upper borders.
+        -n_cases: number of cases taken for each sample (each sample represents
+                the total sum of a dimension). A case is a combination of
+                variables where all summed together equals the sample.
+        -divs: number of divisions in that dimension. It will be the growth
+                order of the number of cells
+        -lower: lower bound of the dimension (minimum value of a sample)
+        -upper: upper bound of the dimension (maximum value of a sample)
+        -label: dimension identifier
 
         Apart from that, it can also be specified the number of samplesand the
         tolerance (maximum difference upper-lower bound of a dimension within a
