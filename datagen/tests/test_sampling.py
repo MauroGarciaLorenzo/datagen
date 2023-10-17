9e86fa4c69e7b36d23ce77d3b400d544fc12b6d3
mgarci5 <mgarci5@bsc.es>
2023-10-17 16:03:44 +0200

    Add tests

--- datagen/tests/test_sampling.py
+++ datagen/tests/test_sampling.py
@@ -1,12 +1,102 @@
 
 from unittest import TestCase
 
-from datagen import generate_columns, Dimension
+from datagen import *
 
 
 class Test(TestCase):
 
+    def setUp(self):
+        """
+        This code executes every time a subtest of this class is run.
+        """
+        variables = [(0, 10), (0, 15), (0, 20), (0, 25)]
+
+        self.dim1 = Dimension(variables, n_cases=3, divs=1, borders=(0, 70),
+                              label="Dim1")
+        self.dim2 = Dimension(variables, n_cases=3, divs=2, borders=(0, 70),
+                              label="Dim2")
+        self.dims = [self.dim1, self.dim2]
+
+        self.children_grid = gen_grid([self.dim1, self.dim2])
+
+        data = {
+            "Dim1": [10, 35, 70],
+            "Dim2": [15, 40, 80]
+        }
+        self.dims_df = pd.DataFrame(data)
+
+        self.cases_heritage_df = pd.DataFrame({
+            "Dim1_Var0": [5, 15, 35],
+            "Dim1_Var1": [2, 8, 20],
+            "Dim1_Var2": [2, 6, 10],
+            "Dim1_Var3": [1, 6, 5],
+            "Dim2_Var0": [5, 10, 40],
+            "Dim2_Var1": [5, 10, 20],
+            "Dim2_Var2": [3, 10, 15],
+            "Dim2_Var3": [2, 10, 5]
+        })
+
     def test_generate_columns(self):
-        dim = Dimension(,
-        cols = generate_columns(dim)
-        self.assertTrue(len(cols), ["Dim1", "Dim2", "Dim3"])
+        cols = generate_columns(self.dim1)
+        self.assertTrue(cols,
+                        ["Dim1_Var0", "Dim1_Var1", "Dim1_Var2", "Dim1_Var3"])
+
+    def test_gen_samples(self):
+        n_samples = 100
+        dimensions = self.dims
+        df_samples = gen_samples(n_samples, dimensions)
+
+        for dim in dimensions:
+            self.assertTrue(all(df_samples[dim.label] >= dim.borders[0]))
+            self.assertTrue(all(df_samples[dim.label] <= dim.borders[1]))
+
+    def test_gen_grid(self):
+        grid = gen_grid(self.dims)
+
+        expected_cells = 1 * 2
+        self.assertEqual(len(grid), expected_cells)
+
+        for i, cell in enumerate(grid):
+            for dim, expected_dim in zip(cell.dimensions, self.dims):
+                expected_upper = expected_dim.borders[
+                                     1] / expected_dim.divs * (i + 1)
+                expected_lower = expected_dim.borders[
+                                     1] / expected_dim.divs * i
+
+                self.assertEqual(dim.borders[0], expected_lower)
+                self.assertEqual(dim.borders[1], expected_upper)
+
+    def test_calculate_entropy(self):
+        freqs = [0.25, 0.75]
+        expected_entropy = -0.25 * np.log(0.25) - 0.75 * np.log(0.75)
+        result_entropy = calculate_entropy(freqs)
+        self.assertAlmostEqual(result_entropy, expected_entropy, places=5)
+
+    def test_eval_entropy(self):
+        stabilities = [1, 0, 0, 0]
+        entropy_parent = None
+        expected_freqs = [0.25, 0.75]
+        expected_entropy = calculate_entropy(expected_freqs)
+        expected_delta_entropy = 1
+
+        result_entropy, result_delta_entropy = eval_entropy(stabilities,
+                                                            entropy_parent)
+
+        self.assertAlmostEqual(result_entropy, expected_entropy, places=5)
+        self.assertEqual(result_delta_entropy, expected_delta_entropy)
+
+    def test_assign_cases(self):
+        total_cases_df, _ = get_children_parameters(self.children_grid,
+                                                    self.dims_df,
+                                                    self.cases_heritage_df)
+
+        # (Dim1: 0-70, Dim2: 0-35)
+        self.assertEqual(len(total_cases_df[0]), 1)
+        pd.testing.assert_frame_equal(total_cases_df[0],
+                                      self.cases_heritage_df.iloc[:1])
+
+        # (Dim1: 0-70, Dim2: 35-70)
+        self.assertEqual(len(total_cases_df[1]), 2)
+        pd.testing.assert_frame_equal(total_cases_df[1],
+                                      self.cases_heritage_df.iloc[1:3])
