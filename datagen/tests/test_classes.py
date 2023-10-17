30f328a0bdefd3f35f856ddb96cc30bab2a9ba1b
iraola <eduiraola@gmail.com>
2023-10-17 09:39:41 +0200

    Add starter code for test_classes.py

---
+++ datagen/tests/test_classes.py
@@ -0,0 +1,31 @@
+
+from unittest import TestCase
+
+import numpy as np
+
+from datagen import Dimension
+
+
+class Test(TestCase):
+    def setUp(self):
+        """
+        This code executes every time a subtest of this class is run.
+        """
+        variables = np.array([(0, 10), (0, 15), (0, 20), (0, 25)])
+        n_cases = 3
+        divs = None
+        lower, upper = 0, 70
+        label = "Test"
+        tolerance = 0.1
+        dim = Dimension(variables, n_cases, divs, (lower, upper), label)
+        dim.tolerance = tolerance
+        self.dim = dim
+
+    def test_get_cases_normal(self):
+        """ Test get_cases_normal method. """
+        ###
+        # Add code here
+
+        self.dim.get_cases_normal(10)
+        self.assertTrue(False)
+        ###
