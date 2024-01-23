cd262648f731239c577d60576cbf440082447e95
FRA uni <francesca.rossi@upc.edu>
2024-01-23 17:15:42 +0100

    'feasible_pf_solution'

--- datagen/src/objective_function.py
+++ datagen/src/objective_function.py
@@ -1,6 +1,8 @@
 import math
 import time
 import numpy as np
+import copy
+
 
 from .utils import get_case_results
 from stability_analysis.operating_point_from_datagenerator import datagen_OP
@@ -10,6 +12,9 @@ from stability_analysis.preprocess import preprocess_data, read_data, process_ra
 from stability_analysis.state_space import generate_NET, build_ss, generate_elements
 from stability_analysis.analysis import small_signal
 
+from stability_analysis.powerflow import check_feasibility
+
+from GridCalEngine.Simulations.PowerFlow.power_flow_options import ReactivePowerControlMode, SolverType
 
 # file where objective function is declared (dummy test)
 def dummy(case, **kwargs):
@@ -28,8 +33,8 @@ def dummy_linear(case, **kwargs):
     total_sum = sum(case)
     return total_sum/19 > 0.5, {}  # 19 => maximum value among all upper borders
 
-
-def small_signal_stability(case, **kwargs):
+# @task(return=3)
+def feasible_power_flow(case, **kwargs):
     d_raw_data = kwargs.get("d_raw_data", None)
     d_op = kwargs.get("d_op", None)
     GridCal_grid = kwargs.get("GridCal_grid", None)
@@ -41,12 +46,12 @@ def small_signal_stability(case, **kwargs):
                                                             d_op)
     
     d_raw_data, slack_bus_num = choose_slack_bus(d_raw_data)
-    
+    # slack_bus_num=80
     assign_SlackBus_to_grid.assign_slack_bus(GridCal_grid, slack_bus_num)
-    assign_Generators_to_grid.assign_StaticGen(GridCal_grid, d_raw_data, d_op)
+    assign_Generators_to_grid.assign_PVGen(GridCal_grid, d_raw_data, d_op)
     assign_PQ_Loads_to_grid.assign_PQ_load(GridCal_grid, d_raw_data)
 
-    # %% POWER-FLOW
+    # %% Run 1st POWER-FLOW
 
     # Receive system status from OPAL
     # d_grid, GridCal_grid, data_old = process_opal.update_OP_from_RT(d_grid, GridCal_grid, data_old)
@@ -55,12 +60,193 @@ def small_signal_stability(case, **kwargs):
     pf_results = GridCal_powerflow.run_powerflow(GridCal_grid)
 
     print('Converged:', pf_results.convergence_reports[0].converged_[0])
+    
 
     # Update PF results and operation point of generator elements
     d_pf = process_powerflow.update_OP(GridCal_grid, pf_results, d_raw_data)
+    
+    d_pf_original=copy.deepcopy(d_pf)
+    
+    if pf_results.convergence_reports[0].converged_[0] == False:
+        return None, None, None
+    # check violations
+    Bus_voltage_violation, Gen_Q_Min_limit_violation, Gen_Q_Max_limit_violation, Gen_pf_violation, Gen_V_violation = check_feasibility.check_violations(d_pf, d_op)
+
+    # if len(Gen_pf_violation)==0:
+    #     break
+
+#%% IF UNFEASIBLE
+    
+    num_gen_viol=len(Gen_pf_violation)
+    d_raw_data_gen_prev_iter=d_raw_data['generator'].copy(deep=True)
+
+    print('min cosphi',min(Gen_pf_violation['Val']))
+    print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+    n_iter=0
+    while len(Gen_pf_violation)>0 or n_iter>100:
+        
+        if len(Gen_pf_violation)>1 and d_pf['pf_gen'].loc[d_pf['pf_gen'].query('bus == @slack_bus_num').index[0],'P']>0:
+            
+            d_raw_data, d_raw_data_gen_prev_iter,num_gen_viol=proportional_increase_P(d_pf,d_raw_data, d_op, Gen_pf_violation, slack_bus_num,num_gen_viol,d_raw_data_gen_prev_iter)   
+        
+            assign_Generators_to_grid.assign_PVGen(GridCal_grid, d_raw_data, d_op)
+                        
+            # Get Power-Flow results with GridCal
+            pf_results = GridCal_powerflow.run_powerflow(GridCal_grid,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+        
+            print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            if pf_results.convergence_reports[0].converged_[0] == False:
+                return None, None, None
+            
+            if pf_results.convergence_reports[0].converged_[0] == False:
+            
+                pf_results = GridCal_powerflow.run_powerflow(GridCal_grid)#,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+                print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+        
+            # Update PF results and operation point of generator elements
+            d_pf = process_powerflow.update_OP(GridCal_grid, pf_results, d_raw_data)
+            
+            Bus_voltage_violation, Gen_Q_Min_limit_violation, Gen_Q_Max_limit_violation, Gen_pf_violation, Gen_V_violation = check_feasibility.check_violations(d_pf, d_op)
+        
+            try:
+                print('min cosphi',min(Gen_pf_violation['Val']))
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+            except: 
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+                
+        if len(Gen_pf_violation)>1 and d_pf['pf_gen'].loc[d_pf['pf_gen'].query('bus == @slack_bus_num').index[0],'P']<0:
+            
+            d_raw_data['generator'] = d_raw_data_gen_prev_iter
+            
+            assign_Generators_to_grid.assign_PVGen(GridCal_grid, d_raw_data, d_op)
+             
+            # Get Power-Flow results with GridCal
+            pf_results = GridCal_powerflow.run_powerflow(GridCal_grid,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+         
+            print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            if pf_results.convergence_reports[0].converged_[0] == False:
+            
+                pf_results = GridCal_powerflow.run_powerflow(GridCal_grid)#,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+                print('Converged:',pf_results.convergence_reports[0].converged_[0])            
+         
+            # Update PF results and operation point of generator elements
+            d_pf = process_powerflow.update_OP(GridCal_grid, pf_results, d_raw_data)
+            
+            Bus_voltage_violation, Gen_Q_Min_limit_violation, Gen_Q_Max_limit_violation, Gen_pf_violation, Gen_V_violation = check_feasibility.check_violations(d_pf, d_op)
+         
+            try:
+                print('min cosphi',min(Gen_pf_violation['Val']))
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+            except: 
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+             
+            Gen_high_cosphi=d_pf['pf_gen'].query('cosphi >0.98').reset_index(drop=True)
+            
+            d_raw_data, d_raw_data_gen_prev_iter = proportional_deincrease_P(d_pf,d_raw_data,d_op,Gen_high_cosphi)
+            assign_Generators_to_grid.assign_PVGen(GridCal_grid, d_raw_data, d_op)
+             
+            
+            # Get Power-Flow results with GridCal
+            pf_results = GridCal_powerflow.run_powerflow(GridCal_grid,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+            print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            if pf_results.convergence_reports[0].converged_[0] == False:
+            
+                pf_results = GridCal_powerflow.run_powerflow(GridCal_grid)#,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+                print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            
+            # Update PF results and operation point of generator elements
+            d_pf = process_powerflow.update_OP(GridCal_grid, pf_results, d_raw_data)
+            
+            Bus_voltage_violation, Gen_Q_Min_limit_violation, Gen_Q_Max_limit_violation, Gen_pf_violation, Gen_V_violation = check_feasibility.check_violations(d_pf, d_op)
+            
+            try:
+                print('min cosphi',min(Gen_pf_violation['Val']))
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+            except: 
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+             
+        if len(Gen_pf_violation)==1 and Gen_pf_violation.loc[0,'GenBus']==slack_bus_num:    
+            d_raw_data, d_raw_data_gen_prev_iter = deincrease_P_only_1Gen(d_pf,d_raw_data,d_op)
+            assign_Generators_to_grid.assign_PVGen(GridCal_grid, d_raw_data, d_op)
+             
+            
+            # Get Power-Flow results with GridCal
+            pf_results = GridCal_powerflow.run_powerflow(GridCal_grid,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+            print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            if pf_results.convergence_reports[0].converged_[0] == False:
+            
+                pf_results = GridCal_powerflow.run_powerflow(GridCal_grid)#,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+                print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            
+            # Update PF results and operation point of generator elements
+            d_pf = process_powerflow.update_OP(GridCal_grid, pf_results, d_raw_data)
+            
+            Bus_voltage_violation, Gen_Q_Min_limit_violation, Gen_Q_Max_limit_violation, Gen_pf_violation, Gen_V_violation = check_feasibility.check_violations(d_pf, d_op)
+            
+            try:
+                print('min cosphi',min(Gen_pf_violation['Val']))
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+            except: 
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+             
+        if len(Gen_pf_violation)==1 and Gen_pf_violation.loc[0,'GenBus']!=slack_bus_num:    
+            d_raw_data, d_raw_data_gen_prev_iter,num_gen_viol=proportional_increase_P(d_pf,d_raw_data, d_op, Gen_pf_violation, slack_bus_num,num_gen_viol,d_raw_data_gen_prev_iter)   
+            assign_Generators_to_grid.assign_PVGen(GridCal_grid, d_raw_data, d_op)
+             
+            
+            # Get Power-Flow results with GridCal
+            pf_results = GridCal_powerflow.run_powerflow(GridCal_grid,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+            print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            if pf_results.convergence_reports[0].converged_[0] == False:
+            
+                pf_results = GridCal_powerflow.run_powerflow(GridCal_grid)#,SolverType.IWAMOTO,ReactivePowerControlMode.Iterative)
+            
+                print('Converged:',pf_results.convergence_reports[0].converged_[0])
+            
+            
+            # Update PF results and operation point of generator elements
+            d_pf = process_powerflow.update_OP(GridCal_grid, pf_results, d_raw_data)
+            
+            Bus_voltage_violation, Gen_Q_Min_limit_violation, Gen_Q_Max_limit_violation, Gen_pf_violation, Gen_V_violation = check_feasibility.check_violations(d_pf, d_op)
+            
+            try:
+                print('min cosphi',min(Gen_pf_violation['Val']))
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+            except: 
+                print('P slack', d_pf['pf_gen'].query('bus == @slack_bus_num')['P'])
+             
+
+        n_iter=n_iter+1
+    
+    # d_raw_data=update_raw_data(d_pf)    
+    return d_pf_original, d_pf, d_raw_data 
 
     # %% FILL d_grid
 
+def small_signal_stability(case, **kwargs):
+    d_raw_data = kwargs.get("d_raw_data", None)
+    d_op = kwargs.get("d_op", None)
+    GridCal_grid = kwargs.get("GridCal_grid", None)
+    d_grid = kwargs.get("d_grid", None)
+    d_sg = kwargs.get("d_sg", None)
+    d_vsc = kwargs.get("d_vsc", None)
+    d_pf = kwargs.get("d_pf", None)
+    
     d_grid, d_pf = fill_d_grid_after_powerflow.fill_d_grid(d_grid,
                                                            GridCal_grid, d_pf,
                                                            d_raw_data, d_op)
@@ -153,4 +339,76 @@ def choose_slack_bus(d_raw_data):
         raise RuntimeError('Error: missing generator at slack bus')
     return d_raw_data, slack_bus
 
+def proportional_increase_P(d_pf,d_raw_data,d_op,Gen_pf_violation,slack_bus_num, num_gen_viol,d_raw_data_gen_prev_iter):
+    min_cosphi=min(Gen_pf_violation.query('GenBus != @slack_bus_num')['Val'])
+    max_delta_cosphi=0.95-min_cosphi
     
+    if len(Gen_pf_violation)<=num_gen_viol:
+        
+        d_raw_data_gen_prev_iter=d_raw_data['generator'].copy(deep=True)
+        num_gen_viol=len(Gen_pf_violation)
+
+    # d_raw_data_gen_prev_iter=d_raw_data['generator'].copy(deep=True)
+    # num_gen_viol=len(Gen_pf_violation)
+    for i in range(len(Gen_pf_violation)):
+        cosphi=Gen_pf_violation.loc[i,'Val']
+        delta_cosphi=0.95-cosphi
+        delta_P=0.1/max_delta_cosphi*delta_cosphi
+        
+        bus=Gen_pf_violation.loc[i,'GenBus']
+        P_pu= d_pf['pf_gen'].loc[d_pf['pf_gen'].query('bus==@bus').index[0],'P']*100/d_op['Generators'].loc[d_op['Generators'].query('BusNum == @bus').index[0],'Pmax']
+
+        if P_pu+delta_P<1:
+            P_pu=P_pu+delta_P
+        else:
+            P_pu=1
+        d_raw_data['generator'].loc[d_raw_data['generator'].query('I ==@bus').index[0],'PG']=P_pu*d_op['Generators'].loc[d_op['Generators'].query('BusNum == @bus').index[0],'Pmax']
+
+    return d_raw_data, d_raw_data_gen_prev_iter,num_gen_viol
+    
+
+def proportional_deincrease_P(d_pf,d_raw_data,d_op,Gen_high_cosphi):
+    max_cosphi=max(Gen_high_cosphi['cosphi'])
+    max_delta_cosphi=max_cosphi- 0.95
+    d_raw_data_gen_prev_iter=d_raw_data['generator'].copy(deep=True)
+    
+    for i in range(len(Gen_high_cosphi)):
+        cosphi=Gen_high_cosphi.loc[i,'cosphi']
+        delta_cosphi=cosphi-0.95
+        delta_P=0.1/max_delta_cosphi*delta_cosphi
+        
+        
+        bus=Gen_high_cosphi.loc[i,'bus']
+        P_pu= d_pf['pf_gen'].loc[d_pf['pf_gen'].query('bus==@bus').index[0],'P']*100/d_op['Generators'].loc[d_op['Generators'].query('BusNum == @bus').index[0],'Pmax']
+
+        if P_pu-delta_P>0:
+            P_pu=P_pu-delta_P
+        else:
+            P_pu=0
+        d_raw_data['generator'].loc[d_raw_data['generator'].query('I ==@bus').index[0],'PG']=P_pu*d_op['Generators'].loc[d_op['Generators'].query('BusNum == @bus').index[0],'Pmax']
+
+    return d_raw_data, d_raw_data_gen_prev_iter
+       
+   
+def deincrease_P_only_1Gen(d_pf,d_raw_data,d_op):
+    max_cosphi=max(d_pf['pf_gen']['cosphi'])
+    i_max=np.argmax(d_pf['pf_gen']['cosphi'])
+    max_delta_cosphi=max_cosphi- 0.95
+    d_raw_data_gen_prev_iter=d_raw_data['generator'].copy(deep=True)
+    
+    delta_P=0.05
+        
+    bus=d_pf['pf_gen'].loc[i_max,'bus']
+    P_pu= d_pf['pf_gen'].loc[d_pf['pf_gen'].query('bus==@bus').index[0],'P']*100/d_op['Generators'].loc[d_op['Generators'].query('BusNum == @bus').index[0],'Pmax']
+
+    if P_pu-delta_P>0:
+        P_pu=P_pu-delta_P
+    else:
+        P_pu=0
+    d_raw_data['generator'].loc[d_raw_data['generator'].query('I ==@bus').index[0],'PG']=P_pu*d_op['Generators'].loc[d_op['Generators'].query('BusNum == @bus').index[0],'Pmax']
+
+    return d_raw_data, d_raw_data_gen_prev_iter
+   
+def update_raw_data(d_raw_data,d_pf):
+    
+    return d_raw_data
