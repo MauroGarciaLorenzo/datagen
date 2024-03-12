f11dbf3a9afe4726d626fab96f28bbbd37b1e38f
marcelosaurus <marcel.garrobe@upc.edu>
2024-03-12 12:57:18 +0100

    symbolic

---
+++ datagen/src/objective_function_ACOPF_symb.py
@@ -0,0 +1,255 @@
+import math
+import time
+import numpy as np
+import copy
+
+from datagen.src import sampling
+
+from .utils import get_case_results
+from stability_analysis.operating_point_from_datagenerator import datagen_OP
+from stability_analysis.modify_GridCal_grid import assign_Generators_to_grid,assign_PQ_Loads_to_grid, assign_SlackBus_to_grid
+from stability_analysis.powerflow import GridCal_powerflow, process_powerflow, slack_bus, fill_d_grid_after_powerflow
+from stability_analysis.preprocess import preprocess_data, read_data, process_raw, parameters,read_op_data_excel, admittance_matrix
+from stability_analysis.state_space import generate_NET, build_ss, generate_elements
+from stability_analysis.analysis import small_signal
+
+from stability_analysis.optimal_power_flow import process_optimal_power_flow
+from stability_analysis.powerflow import check_feasibility
+
+from GridCalEngine.Simulations.PowerFlow.power_flow_options import ReactivePowerControlMode, SolverType
+from GridCalEngine.Simulations.OPF.NumericalMethods import ac_opf
+from GridCalEngine.Simulations.OPF.NumericalMethods.ac_opf import run_nonlinear_opf, ac_optimal_power_flow
+from GridCalEngine.Core.DataStructures.numerical_circuit import compile_numerical_circuit_at
+import GridCalEngine.api as gce
+
+from .utils_obj_fun import *
+
+import time
+
+import sympy as sp
+
+def feasible_power_flow_ACOPF(case,N_pf, tau_droop_f_gfor_12, **kwargs):
+    d_raw_data = kwargs.get("d_raw_data", None)
+    d_op = kwargs.get("d_op", None)
+    GridCal_grid = kwargs.get("GridCal_grid", None)
+    d_grid = kwargs.get("d_grid", None)
+    d_sg = kwargs.get("d_sg", None)
+    d_vsc = kwargs.get("d_vsc", None)
+    voltage_profile = kwargs.get("voltage_profile", None)
+    v_min_v_max_delta_v = kwargs.get("v_min_v_max_delta_v", None)
+    V_set = kwargs.get("V_set", None)
+    
+    computing_times=dict()
+
+
+    if voltage_profile != None and v_min_v_max_delta_v == None:
+        print('Error: Voltage profile option selected but v_min, v_max, and delta_v are missing')
+        return None, None, None
+
+    if voltage_profile != None and V_set != None:
+        print('Error: Both Voltage profile and V_set option is selected. Choose only one of them')
+        return None, None, None
+
+    if voltage_profile == None and V_set == None:
+        print('Error: Neither Voltage profile or V_set option is selected. Choose one of them')
+        return None, None, None
+
+    d_raw_data, d_op = datagen_OP.generated_operating_point(case, d_raw_data,
+                                                            d_op)
+    d_raw_data, slack_bus_num = choose_slack_bus(d_raw_data)
+    i_slack=int(d_raw_data['generator'].query('I == @slack_bus_num').index[0])
+
+    # slack_bus_num=80
+    assign_SlackBus_to_grid.assign_slack_bus(GridCal_grid, slack_bus_num)
+
+    if voltage_profile != None:
+        vmin = v_min_v_max_delta_v[0]
+        vmax = v_min_v_max_delta_v[1]
+        delta_v = v_min_v_max_delta_v[2]
+
+        voltage_profile_list, indx_id = sampling.gen_voltage_profile(vmin, vmax, delta_v, d_raw_data, slack_bus_num,
+                                                                     GridCal_grid)
+
+        assign_Generators_to_grid.assign_PVGen(GridCal_grid=GridCal_grid, d_raw_data=d_raw_data, d_op=d_op,
+                                               voltage_profile_list=voltage_profile_list, indx_id=indx_id)
+
+    elif V_set != None:
+        assign_Generators_to_grid.assign_PVGen(GridCal_grid=GridCal_grid, d_raw_data=d_raw_data, d_op=d_op, V_set=V_set)
+
+    assign_PQ_Loads_to_grid.assign_PQ_load(GridCal_grid, d_raw_data)
+    
+    # %% Run 1st POWER-FLOW
+
+    # Receive system status from OPAL
+    # d_grid, GridCal_grid, data_old = process_opal.update_OP_from_RT(d_grid, GridCal_grid, data_old)
+
+    # Get Power-Flow results with GridCal
+    pf_results = GridCal_powerflow.run_powerflow(GridCal_grid)
+
+    print('Converged:', pf_results.convergence_reports[0].converged_[0])
+    
+
+    # Update PF results and operation point of generator elements
+    d_pf_original = process_powerflow.update_OP(GridCal_grid, pf_results, d_raw_data)
+    d_pf_original = additional_info_PF_results(d_pf_original, i_slack, pf_results, N_pf)
+
+    nc = compile_numerical_circuit_at(GridCal_grid)
+    nc.generator_data.cost_0[:] = 0
+    nc.generator_data.cost_1[:] = 0
+    nc.generator_data.cost_2[:] = 0
+    pf_options = gce.PowerFlowOptions(solver_type=gce.SolverType.NR, verbose=1, tolerance=1e-8, max_iter=100)
+#    d_opf_results = ac_optimal_power_flow(Pref=np.array(d_pf_original['pf_gen']['P']), slack_bus_num=i_slack, nc=nc, pf_options=pf_options, plot_error=True)
+    
+    start = time.time()
+    
+    d_opf_results = ac_optimal_power_flow(nc=nc, pf_options=pf_options, plot_error=True)
+        
+    end = time.time()
+    computing_times['time_powerflow']=end - start
+    
+    d_opf = process_optimal_power_flow.update_OP(GridCal_grid, d_opf_results, d_raw_data)
+    d_opf = additional_info_OPF_results(d_opf,i_slack, N_pf)
+
+                                        
+    #########################################################################33
+
+
+
+    d_grid, d_opf = fill_d_grid_after_powerflow.fill_d_grid(d_grid,
+                                                           GridCal_grid, d_opf,
+                                                           d_raw_data, d_op)
+
+    # %% READ PARAMETERS
+
+    # Get parameters of generator units from excel files & compute pu base
+    d_grid = parameters.get_params(d_grid, d_sg, d_vsc)
+
+    d_grid = update_control(case, d_grid)
+    
+    d_grid['T_VSC'].loc[d_grid['T_VSC'].query('mode == "GFOL"').index,'k_droop_u']=0.1    
+    d_grid['T_VSC'].loc[d_grid['T_VSC'].query('mode == "GFOL"').index,'k_droop_f']=0.01    
+
+    # Assign slack bus and slack element
+    d_grid = slack_bus.assign_slack(d_grid)
+
+    # Compute reference angle (delta_slk)
+    d_grid, REF_w, num_slk, delta_slk = slack_bus.delta_slk(d_grid)
+
+    # %% GENERATE STATE-SPACE MODEL
+
+    # Generate AC & DC NET State-Space Model
+    start = time.time()
+
+    l_blocks, l_states, d_grid = generate_NET.generate_SS_NET_blocks(d_grid,
+                                                                     delta_slk)
+
+    end = time.time()
+    computing_times['time_generate_SS_net']=end - start
+    
+    start = time.time()
+
+    d_grid['T_VSC'].loc[28,'tau_droop_f']=tau_droop_f_gfor_12
+    # Generate generator units State-Space Model
+    l_blocks, l_states = generate_elements.generate_SS_elements(d_grid,
+                                                                delta_slk,
+                                                                l_blocks,
+                                                                l_states)
+    end = time.time()
+    computing_times['time_generate_SS_elem']=end - start
+   
+    # %% BUILD FULL SYSTEM STATE-SPACE MODEL
+
+    # Define full system inputs and ouputs
+    var_in = ['NET_Rld1']
+    var_out = ['all'] #['all']  # ['GFOR3_w'] #
+
+    # Build full system state-space model
+    start = time.time()
+
+    inputs, outputs = build_ss.select_io(l_blocks, var_in, var_out)
+    ss_sys = build_ss.connect(l_blocks, l_states, inputs, outputs)
+    
+    end = time.time()
+    computing_times['time_connect']=end - start
+  
+
+    # %% SMALL-SIGNAL ANALYSIS
+
+    start = time.time()
+
+    
+    T_EIG = small_signal.FEIG(ss_sys, True)
+    T_EIG.head
+    
+    end = time.time()
+    computing_times['time_eig']=end - start
+ 
+
+    # write to excel
+    # T_EIG.to_excel(path.join(path_results, "EIG_" + excel + ".xlsx"))
+
+    if max(T_EIG['real'] >= 0):
+        stability = 0
+    else:
+        stability = 1
+        
+    # Obtain all participation factors
+    # df_PF = small_signal.FMODAL(ss_sys, plot=False)
+    # # Obtain the participation factors for the selected modes
+    # T_modal, df_PF = small_signal.FMODAL_REDUCED(ss_sys, plot=True, modeID = [1,3,11])
+    # # Obtain the participation factors >= tol, for the selected modes
+    
+    start = time.time()
+
+    T_modal, df_PF = small_signal.FMODAL_REDUCED_tol(ss_sys, plot=True, modeID = np.arange(1,23), tol = 0.3)
+
+    end = time.time()
+    computing_times['time_partfact']=end - start
+    
+   
+    df_op, df_real, df_imag, df_freq, df_damp = (
+        get_case_results(T_EIG=T_EIG, d_grid=d_grid))
+    output_dataframes = {
+        "df_op": df_op, "df_real": df_real, "df_imag": df_imag,
+        "df_freq": df_freq, "df_damp": df_damp
+    }
+    return stability, output_dataframes, d_pf_original, d_opf, d_grid, T_EIG, computing_times
+
+def return_d_opf(d_raw_data, d_opf_results):
+    df_opf_bus = pd.DataFrame(
+        {'bus': d_raw_data['results_bus']['I'], 'Vm': d_opf_results.Vm, 'theta': d_opf_results.Va})
+    df_opf_gen_pre = pd.DataFrame(
+        {'bus': d_raw_data['generator']['I'], 'P': d_opf_results.Pg, 'Q': d_opf_results.Qg})
+    df_opf_gen = pd.merge(df_opf_gen_pre, df_opf_bus[['bus', 'Vm', 'theta']], on='bus', how='left')
+    d_opf = {'opf_bus': df_opf_bus, 'opf_gen': df_opf_gen,'opf_load': d_raw_data['load']} 
+    return d_opf
+
+
+def write_csv(d, path, N_pf, filename_start):
+    for df_name, df in d.items():
+        filename=filename_start+'_'+str(N_pf)+'_'+str(df_name)
+        pd.DataFrame.to_csv(df,path+filename+".csv")
+        
+def write_xlsx(d, path, filename):
+    with pd.ExcelWriter(path+filename+".xlsx") as writer:
+        for df_name, df in d.items():
+            if isinstance(df, pd.DataFrame):                
+                df.to_excel(writer, sheet_name=df_name)
+
+
+def update_control(case, d_grid):
+    case_index=case.index
+
+    for i in range(0,len(d_grid['T_VSC'])):
+        mode=d_grid['T_VSC'].loc[i,'mode']
+        bus=d_grid['T_VSC'].loc[i,'bus']
+        
+        control_p_mode=[cc for cc in case.index if 'tau' and mode.lower() in cc]
+        control_p_mode_bus=[cc for cc in control_p_mode if str(bus) in cc]
+        
+        control_p_labels=[''.join(filter(lambda x: not x.isdigit(), cc))[:-1].replace(mode.lower(),'')[:-1] for cc in control_p_mode_bus ]
+        
+        for control_p,control_p_bus in zip(control_p_labels,control_p_mode_bus):
+            d_grid['T_VSC'].loc[i,control_p]=case[control_p_bus]
+    
+    return d_grid
---
+++ run_N_PF_symb.py
@@ -0,0 +1,321 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from datagen.src.dimensions import Dimension
+from datagen.src.start_app import start
+from datagen.src.objective_function_ACOPF import *
+
+# from datagen.src.objective_function import small_signal_stability
+
+try:
+    from pycompss.api.task import task
+    from pycompss.api.api import compss_wait_on
+except ImportError:
+    from datagen.dummies.task import task
+    from datagen.dummies.api import compss_wait_on
+
+from os import path, getcwd
+from stability_analysis.data import get_data_path
+from stability_analysis.preprocess import preprocess_data, read_data, \
+    process_raw, parameters, admittance_matrix
+from stability_analysis.powerflow import GridCal_powerflow, process_powerflow, \
+    slack_bus, fill_d_grid_after_powerflow
+from stability_analysis.state_space import generate_NET, build_ss, \
+    generate_elements
+from stability_analysis.opal import process_opal
+from stability_analysis.analysis import small_signal
+from stability_analysis.preprocess.utils import *
+# from stability_analysis.random_operating_point import random_OP
+from stability_analysis.modify_GridCal_grid import assign_Generators_to_grid, \
+    assign_PQ_Loads_to_grid
+# from GridCalEngine.Core.DataStructures import numerical_circuit
+
+# from datagen.src.SS_stab import small_signal_stability
+
+
+# %% SET FILE NAMES AND PATHS
+
+# Paths to data
+
+path_data = get_data_path()
+path_raw = path.join(path_data, "raw")
+path_results = path.join(path_data, "results")
+
+# File names
+
+gridname='IEEE118'#'IEEE9'#
+
+if gridname == 'IEEE9':
+# # # IEEE 9
+    raw = "ieee9_6"
+    excel = "IEEE_9_headers" 
+    excel_data = "IEEE_9" 
+    excel_op = "OperationData_IEEE_9"
+
+elif gridname=='IEEE118':
+    # IEEE 118 
+    raw = "IEEE118busREE_Winter_Solved_mod_PQ_91Loads"
+    # excel = "IEEE_118bus_TH" # THÉVENIN
+    # excel = "IEEE_118_01" # SG
+    excel = "IEEE_118_FULL_headers" 
+    excel_data = "IEEE_118_FULL" 
+    excel_op = "OperationData_IEEE_118"
+    excel_lines_ratings = "IEEE_118_Lines"
+
+# TEXAS 2000 bus
+# raw = "ACTIVSg2000_solved_noShunts"
+# excel = "texas_2000"
+
+
+raw_file = path.join(path_raw, raw + ".raw")
+# excel_raw = path.join(path_raw, raw + ".xlsx")
+excel_sys = path.join(path_data, "cases/" + excel + ".xlsx") #empty 
+excel_sg = path.join(path_data, "cases/" + excel_data + "_data_sg.xlsx") 
+excel_vsc = path.join(path_data, "cases/" + excel_data + "_data_vsc.xlsx") 
+excel_op = path.join(path_data, "cases/" + excel_op + ".xlsx") 
+
+if gridname == 'IEEE118':
+    excel_lines_ratings = path.join(path_data, "cases/" + excel_lines_ratings + ".csv")
+
+# %% READ OPERATION EXCEL FILE
+
+d_op = read_data.read_data(excel_op)
+
+# %% READ RAW FILE
+
+# Read raw file
+d_raw_data = process_raw.read_raw(raw_file)
+
+if gridname == 'IEEE9':
+    # For the IEEE 9-bus system
+    d_raw_data['generator']['Region']=1
+    d_raw_data['load']['Region']=1
+    d_raw_data['branch']['Region']=1
+    d_raw_data['results_bus']['Region']=1
+
+elif gridname == 'IEEE118':
+    # FOR the 118-bus system
+    d_raw_data['generator']['Region']=d_op['Generators']['Region']
+    d_raw_data['load']['Region']=d_op['Loads']['Region']
+    # d_raw_data['branch']['Region']=1
+    d_raw_data['results_bus']['Region']=d_op['Buses']['Region']
+    d_raw_data['generator']['MBASE']=d_op['Generators']['Snom']
+
+    lines_ratings=pd.read_csv(excel_lines_ratings)
+
+
+# Preprocess input raw data to match excel file format
+preprocess_data.preprocess_raw(d_raw_data)
+
+# Write to excel file
+# preprocess_data.raw2excel(d_raw_data,excel_raw)
+
+#%% Create GridCal Model
+GridCal_grid = GridCal_powerflow.create_model(path_raw, raw_file)
+
+for line in GridCal_grid.lines:
+    bf = int(line.bus_from.code)
+    bt = int(line.bus_to.code)
+
+    line.rate=lines_ratings.loc[lines_ratings.query('Bus_from == @bf and Bus_to == @bt').index[0],'Max Flow (MW)']
+    # print(line.rate)
+
+for trafo in GridCal_grid.transformers2w:
+    bf = int(trafo.bus_from.code)
+    bt = int(trafo.bus_to.code)
+
+    trafo.rate=lines_ratings.loc[lines_ratings.query('Bus_from == @bf and Bus_to == @bt').index[0],'Max Flow (MW)']
+    # print(trafo.rate)
+
+# %% READ EXCEL FILE
+
+# Read data of grid elements from Excel file
+d_grid, d_grid_0 = read_data.read_sys_data(excel_sys)
+
+# TO BE DELETED
+d_grid = read_data.tempTables(d_grid)
+
+# # Read simulation configuration parameters from Excel file
+# sim_config = read_data.get_simParam(excel_sys)
+
+# %% READ EXEC FILES WITH SG AND VSC CONTROLLERS PARAMETERS
+
+d_sg = read_data.read_data(excel_sg)
+
+d_vsc = read_data.read_data(excel_vsc)
+
+# %%
+# @task()
+# def main():
+"""
+In this method we work with dimensions (main axes), which represent a
+list of variable_borders. For example, the value of each variable of a concrete
+dimension could represent the power supplied by a generator, while the
+value linked to that dimension should be the total sum of energy produced.
+
+For each dimension it must be declared:
+    -variable_borders: list of variable_borders represented by tuples containing its
+            lower and upper borders.
+    -n_cases: number of cases taken for each sample (each sample represents
+            the total sum of a dimension). A case is a combination of
+            variable_borders where all summed together equals the sample.
+    -divs: number of divisions in that dimension. It will be the growth
+            order of the number of cells
+    -lower: lower bound of the dimension (minimum value of a sample)
+    -upper: upper bound of the dimension (maximum value of a sample)
+    -label: dimension identifier
+    -independent_dimension: indicates whether the dimension is true (sampleable) or not.
+
+Apart from that, it can also be specified the number of samples and
+the relative tolerance (indicates the portion of the size of the original
+dimension). For example, if we have a dimension of size 10 and relative
+tolerance is 0.5, the smallest cell in this dimension will have size 5.
+Lastly, user should provide the objective function. As optional parameters,
+the user can define:
+    -use_sensitivity: a boolean indicating whether sensitivity analysis is
+    used or not.
+    -ax: plot axes in case it is desired to show stability points and cells
+    divisions (dimensions length must be 2). Plots saved in
+    "datagen/results/figures".
+    -plot_boxplot: a boolean indicating whether boxplots for each variable
+    must be obtained or not. Plots saved in "datagen/results/figures".
+"""
+
+# p_sg = [(0, 2), (0, 1.5), (0, 1.5)]
+# p_cig = [(0, 1), (0, 1.5), (0, 1.5), (0, 2)]
+# tau_f_g_for = [(0., 2)]
+# tau_v_g_for = [(0., 2)]
+# tau_p_g_for = [(0., 2)]
+# tau_q_g_for = [(0., 2)]
+
+p_sg = [
+    (d_op['Generators']['Pmin_SG'].iloc[i], d_op['Generators']['Pmax_SG'].iloc[i])
+    for i in range(len(d_op['Generators']))]
+p_cig = [(d_op['Generators']['Pmin_CIG'].iloc[i],
+          d_op['Generators']['Pmax_CIG'].iloc[i]) for i in
+         range(len(d_op['Generators']))]
+p_loads = list(d_op['Loads']['Load_Participation_Factor'])
+
+loads_power_factor = 0.98
+generators_power_factor = 0.98
+
+tau_f_g_for = [(0., 2)]
+tau_v_g_for = [(0., 2)]
+tau_p_g_for = [(0., 2)]
+tau_q_g_for = [(0., 2)]
+
+n_samples = 1
+n_cases = 1
+
+rel_tolerance = 0.01
+max_depth = 3
+dimensions = dict()
+dimensions = [
+      Dimension(label="p_sg", variable_borders=p_sg,
+                n_cases=n_cases, divs=2,
+                borders=(d_op['Generators']['Pmin_SG'].sum(),
+                d_op['Generators']['Pmax_SG'].sum()),
+                independent_dimension=True, cosphi=generators_power_factor),
+      Dimension(label="p_cig", variable_borders=p_cig,
+                n_cases=n_cases, divs=1,
+                borders=(d_op['Generators']['Pmin_CIG'].sum(),
+                d_op['Generators']['Pmax_CIG'].sum()),
+                independent_dimension=True,
+                cosphi=generators_power_factor),
+      Dimension(label="perc_g_for", variable_borders=[(0,1)],
+                n_cases=n_cases, divs=1, borders=(0, 1),
+                independent_dimension=True, cosphi=None),
+      Dimension(label="p_load", values=p_loads,
+                n_cases=n_cases, divs=1,
+                independent_dimension=False,
+                cosphi=loads_power_factor)
+              ]
+
+for d in list(d_op['Generators']['BusNum']):
+    dimensions.append(Dimension(label='tau_droop_f_gfor_'+str(d), n_cases=n_cases,
+                                divs=1, borders=(1/50,1/10),
+                                independent_dimension=True,
+                                cosphi=None))
+    
+    dimensions.append(Dimension(label='tau_droop_u_gfor_'+str(d), n_cases=n_cases,
+                                divs=1, borders=(1/50,1/10),
+                                independent_dimension=True,
+                                cosphi=None))
+    
+    dimensions.append(Dimension(label='tau_droop_f_gfol_'+str(d), n_cases=n_cases,
+                                divs=1, borders=(1/50,1/10),
+                                independent_dimension=True,
+                                cosphi=None))
+    
+    dimensions.append(Dimension(label='tau_droop_u_gfol_'+str(d), n_cases=n_cases,
+                                divs=1, borders=(1/50,1/10),
+                                independent_dimension=True,
+                                cosphi=None))
+    
+
+#%%
+
+from datagen.src.sampling import gen_samples
+from datagen.src.sampling import gen_cases
+# from datagen.src.objective_function import *
+
+seed=10
+
+generator = np.random.default_rng(seed)
+
+samples_df = gen_samples(n_samples, dimensions, generator)
+# Generate cases (n_cases (attribute of the class Dimension) for each dim)
+
+tau_droop_f_gfor_12=sp.Symbol('tau_droop_f_gfor_12')
+cases_df, dims_df = gen_cases(samples_df, dimensions, generator)
+
+voltage_profile=True
+v_min_v_max_delta_v=[0.95,1.05,0.02]
+
+# V_set=0.95
+
+#%%
+N_pf=1
+for _, case in cases_df.iterrows():
+    stability, output_dataframes, d_pf_original, d_opf, d_grid, T_EIG, computing_times = feasible_power_flow_ACOPF(case=case,
+                                                                                    N_pf=N_pf,
+                                                                                    tau_droop_f_gfor_12,
+                                                                                    d_raw_data=d_raw_data,
+                                                                                    d_op=d_op,
+                                                                                    GridCal_grid=GridCal_grid,
+                                                                                    d_grid=d_grid, d_sg=d_sg,
+                                                                                    d_vsc=d_vsc,
+                                                                                    voltage_profile=voltage_profile,
+                                                                                    v_min_v_max_delta_v=v_min_v_max_delta_v
+                                                                                    # V_set=V_set
+                                                                                    )
+    
+    
+    N_pf=N_pf+1
+
+# control='_gfol_kf_001_kv_01'
+# path='./datagen/src/results/'
+# write_csv(d_pf_original, path, seed, 'PF_orig'+control)
+# write_xlsx(d_opf, path, 'OPF_'+str(seed)+control)
+# write_csv(output_dataframes, path, seed, 'OPF'+control)
+# write_xlsx(d_grid,path,'d_grid_'+str(seed)+control)
+
+    # d_pf_original, d_pf, d_raw_data = feasible_power_flow(case=case,
+    #                                          d_raw_data=d_raw_data,
+    #                                          d_op=d_op,
+    #                                          GridCal_grid=GridCal_grid,
+    #                                          d_grid=d_grid, d_sg=d_sg,
+    #                                          d_vsc=d_vsc,
+    #                                          # voltage_profile=voltage_profile,
+    #                                          # v_min_v_max_delta_v=v_min_v_max_delta_v
+    #                                          V_set=V_set
+    #                                          )
+
+# stability, output_dataframes= small_signal_stability(case=case, d_raw_data = d_raw_data,
+#                                                      d_op = d_op,
+#                                                      GridCal_grid = GridCal_grid,
+#                                                      d_grid = d_grid,
+#                                                      d_sg = d_sg,
+#                                                      d_vsc = d_vsc,
+#                                                      d_pf = d_pf,
+#                                                      )
