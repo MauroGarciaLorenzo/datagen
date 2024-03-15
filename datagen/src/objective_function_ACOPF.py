c2601553b9a300304ae3dbb2beb7dc41a2653659
francesca <francesca.rossi@upc.edu>
2024-03-15 14:39:18 +0100

    comment lines

--- datagen/src/objective_function_ACOPF.py
+++ datagen/src/objective_function_ACOPF.py
@@ -124,8 +124,8 @@ def feasible_power_flow_ACOPF(case,N_pf, **kwargs):
 
     d_grid = update_control(case, d_grid)
     
-    d_grid['T_VSC'].loc[d_grid['T_VSC'].query('mode == "GFOL"').index,'k_droop_u']=0.1    
-    d_grid['T_VSC'].loc[d_grid['T_VSC'].query('mode == "GFOL"').index,'k_droop_f']=0.01    
+    # d_grid['T_VSC'].loc[d_grid['T_VSC'].query('mode == "GFOL"').index,'k_droop_u']=0.1    
+    # d_grid['T_VSC'].loc[d_grid['T_VSC'].query('mode == "GFOL"').index,'k_droop_f']=0.01    
 
     # Assign slack bus and slack element
     d_grid = slack_bus.assign_slack(d_grid)
