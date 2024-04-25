b3b46268f453ef9d4a527193ea162b092f83a388
iraola <eduardo.iraola@bsc.es>
2024-04-25 17:51:14 +0200

    Modify main run_N_PF runner for MN5

--- performance_tests/scripts/run_N_PF.sh
+++ performance_tests/scripts/run_N_PF.sh
@@ -1,26 +1,42 @@
-module load python/3.9.10
-pip install scipy
+export COMPSS_PYTHON_VERSION="3.12.1"
+module load hdf5
+module load sqlite3
+module load python/3.12.1
 module load COMPSs/3.3
 
+# Parse username splitting the string by delimiter "/"
+IFS='/' read -ra parts <<< "$HOME"
+# Get the last element of the array
+username="${parts[-1]}"
+
+# Set up variables and directories
 cd ..
-echo $(pwd)
+current_directory=$(pwd)
+working_dir="/gpfs/scratch/bsc19/$username"
+export PYTHONPATH="${current_directory}/../packages/:${PYTHONPATH}:${current_directory}/../:${current_directory}"
 
-export PYTHONPATH="$(pwd)/.."
+# Print user information
+echo "Username is: $username"
+echo "Using $working_dir as the working directory"
+echo "Current directory is ${current_directory}"
 
+# Run COMPSs execution
 num_nodes=1
-
-while [ ${num_nodes} -le 8 ]
+while [ ${num_nodes} -le 1 ]
 do
   enqueue_compss \
   --pythonpath=${PYTHONPATH} \
-  --job_execution_dir="${current_directory}/.." \
   --num_nodes=${num_nodes} \
-  --worker_working_dir=local_disk \
-  --master_working_dir=local_disk \
+  --job_execution_dir="${current_directory}/.." \
+  --worker_working_dir=${working_dir} \
+  --master_working_dir=${working_dir} \
   --lang=python \
-  --exec_time=15 \
-  --agents \
+  --exec_time=1000 \
   --tracing \
+  --project_name=bsc19 \
+  --qos=gp_bsccs \
+  --log_dir=${working_dir} \
   run_N_PF.py "${current_directory}/../results/run_N_PF_${num_nodes}"
+
   num_nodes=$((num_nodes * 2))
 done
