61091cc57529460827fbfb1d99914f104cf50b04
iraola <eduiraola@gmail.com>
2024-05-03 08:56:20 +0200

    Fix results writing in run_N_PF.py

--- datagen/src/objective_function_ACOPF.py
+++ datagen/src/objective_function_ACOPF.py
@@ -246,6 +246,7 @@ def feasible_power_flow_ACOPF(case,N_pf, **kwargs):
     output_dataframes['d_grid'] = d_grid
     output_dataframes['d_opf'] = d_opf
     output_dataframes['d_pf_original'] = d_pf_original
+    output_dataframes['computing_times'] = pd.Series(computing_times)
     return stability, output_dataframes
 
 def return_d_opf(d_raw_data, d_opf_results):
