ff065419bb3b42b017c8738d09ed1a349b1d10ed
FRA uni <francesca.rossi@upc.edu>
2024-10-16 11:58:26 +0200

    update pp

--- post_processing_standalone/post_processing_standalone.py
+++ post_processing_standalone/post_processing_standalone.py
@@ -1,12 +1,15 @@
 import os
 from utils_pp_standalone import *
 
-dir_name='ACOPF_standalone_seed16_nc3_ns100_20241014_162634_9593'
+ncig_list=['10','11','12']
 path='../results/'
-path_results = os.path.join(path, dir_name)
 
-results_dataframes, csv_files= open_csv(path_results)
-
-perc_stability(results_dataframes['case_df_op'])
+dir_names=[dir_name for dir_name in os.listdir(path) if dir_name.startswith('ACOPF_standalone_NCIG')]
+for dir_name in dir_names:
+    path_results = os.path.join(path, dir_name)
+    
+    results_dataframes, csv_files= open_csv(path_results)
+    
+    perc_stability(results_dataframes['case_df_op'])
 
 
