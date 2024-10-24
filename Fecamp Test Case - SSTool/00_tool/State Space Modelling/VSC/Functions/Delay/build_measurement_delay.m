f9c3bfb6aec815fa1fe68a9924a6baa13169c97f
FRA uni <francesca.rossi@upc.edu>
2024-10-24 11:34:47 +0200

    delete old matlab tool

--- Fecamp Test Case - SSTool/00_tool/State Space Modelling/VSC/Functions/Delay/build_measurement_delay.m
+++
@@ -1,7 +0,0 @@
-function measurement_delay = build_measurement_delay(x,u,y,tau)
-    A= [-1/tau];
-    B = [1/tau];
-    C = 1;
-    D = [0]; 
-    measurement_delay = ss(A,B,C,D,'StateName',x,'inputname',u,'outputname',y);
-end
