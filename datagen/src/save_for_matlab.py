d201fc1f54dfa29725d28e1eaeb97f4420b4cba8
FRA uni <francesca.rossi@upc.edu>
2024-10-30 12:01:12 +0100

    move script save

---
+++ datagen/src/save_for_matlab.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+
+# # Define the path to the folder
+# folder_path = './Fecamp Test Case - SSTool/01_data/cases/NREL_seed'+str(seed)
+
+def save_full(folder_path,filename,d_pf,d_grid):
+
+    #%%
+    # Create the folder if it doesn't exist
+    os.makedirs(folder_path, exist_ok=True)
+    
+    T_PF=d_pf['pf_bus'][['bus','Vm','theta']]
+    with pd.ExcelWriter(folder_path+'/'+filename+'.xlsx', engine='openpyxl') as writer:
+        for key in d_grid:
+            if key!='gen_names':
+                if key=='T_NET':
+                    d_grid[key].to_excel(writer, sheet_name='AC-NET', index=False)
+                elif key=='T_DC_NET':
+                    d_grid[key].to_excel(writer, sheet_name='DC-NET', index=False)
+                else:
+                    d_grid[key].to_excel(writer, sheet_name=key.replace('T_',''), index=False)
+        T_PF.to_excel(writer, sheet_name='PF', index=False)            
+    
+def save_data_vsc(folder_path,filename,d_vsc,d_grid):    
+    with pd.ExcelWriter(folder_path+'/'+filename+'_data_vsc.xlsx', engine='openpyxl') as writer:
+        for mode in ['GFOR','GFOL']:
+            try:
+                T_VSC_data=d_grid['T_VSC'].query('mode ==@mode')[['number','bus']+list(set(d_vsc['User'+mode].columns)-set(['Bac', 'tau_s']))]
+                T_VSC_data['Bac']=d_vsc['User'+mode].loc[0,'Bac']
+                T_VSC_data['tau_s']=d_vsc['User'+mode].loc[0,'tau_s']
+            except: 
+                  T_VSC_data=pd.DataFrame(columns=['number','bus']+list(d_vsc['User'+mode].columns))
+            T_VSC_data.to_excel(writer, sheet_name=mode, index=False)         
+              
