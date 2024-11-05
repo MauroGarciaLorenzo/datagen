1090073873509aa0bcbaee3c7ba995e52e605a1a
FRA uni <francesca.rossi@upc.edu>
2024-11-05 15:16:22 +0100

    add new version of the tool

---
+++ version6_original/version6/Tool/00_tool/Preprocess/set_file_names.m
@@ -0,0 +1,14 @@
+%% SET FILE NAMES
+
+% Create excel file names
+
+excel    = [caseName '.xlsx']; 
+data_sg  = [caseName '_data_sg.xlsx'];
+data_vsc = [caseName '_data_vsc.xlsx'];
+data_ipc = [caseName '_data_ipc.xlsx'];
+data_shunt = [caseName '_data_shunt.xlsx'];
+
+% Simulink models names
+linear    =  [caseNameLin '_LIN'];
+nonlinear = [caseNameNLin];
+
