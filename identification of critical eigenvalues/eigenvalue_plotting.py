df038c5fdaac7a60e20a51a286c5d279bde29b64
Heather Walker <hmacwalker1@gmail.com>
2024-11-20 16:10:13 +0100

    Added Zoomed plots

--- identification of critical eigenvalues/eigenvalue_plotting.py
+++ identification of critical eigenvalues/eigenvalue_plotting.py
@@ -19,9 +19,9 @@ df_imag=pd.read_csv('../results/datagen_ACOPF_LF09_seed17_nc5_ns5_d5_20241119_11
 df_real_clean = df_real.dropna(subset=['2'])
 df_imag_clean = df_imag.dropna(subset=['2'])
 
-# remove the case_id column
-df_real_clean = df_real_clean.drop('case_id', axis=1)
-df_imag_clean = df_imag_clean.drop('case_id', axis=1)
+# remove the first column and the  case_id column
+df_real_clean = df_real_clean.drop([df_real_clean.columns[0], 'case_id', 'Stability'], axis=1)
+df_imag_clean = df_imag_clean.drop([df_imag_clean.columns[0], 'case_id', 'Stability'], axis=1)
 
 n_cases= len(df_real)
 
@@ -38,8 +38,8 @@ fig.tight_layout()
 plt.grid()
 fig.savefig('Modal_Map.png')
 
-# zoom in on the right side
-fig=plt.figure()
+# zoom in
+fig = plt.figure()
 ax=fig.add_subplot()
 ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
 ax.set_xlabel('Real Axis',fontsize=25)
@@ -47,9 +47,10 @@ ax.set_ylabel('Imaginary Axis',fontsize=25)
 ax.set_title('Zoom', fontsize=25)
 ax.tick_params(labelsize=20)
 # ax.set_xlim([-100000,5000])
-ax.set_xlim([-50000,5000])
+ax.set_xlim([-20,20])
+ax.set_ylim([200,300])
 ax.legend(loc='lower center',bbox_to_anchor=(0.45, -0.65),fontsize=15, ncol=2)
 fig.tight_layout()
 plt.grid()
+fig.savefig('Middle_Group_Zoom.png')
 
-fig.savefig('More_Zoomed_Modal_Map.png')
