0afb780e7030a4570caa9a5e917f16d47ea77f73
FRA uni <francesca.rossi@upc.edu>
2024-12-04 11:11:34 +0100

    updates on clustering

--- identification of critical eigenvalues/Critical_eigenvalue_selection_clustering.py
+++ identification of critical eigenvalues/Critical_eigenvalue_selection_clustering.py
@@ -13,6 +13,50 @@ from sklearn.cluster import OPTICS
 from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.cm as cm
 from sklearn.preprocessing import StandardScaler
+import copy
+
+#%%
+
+def exclude_50Hz_eig(real_selected_df, imag_selected_df, plot_figure=True):
+    
+    imag_selected_df=imag_selected_df.round(3)
+
+    imag_selected_no50Hz=pd.DataFrame()
+    real_selected_no50Hz=pd.DataFrame()
+
+    for i in real_selected_df.columns:
+        imag_serie = imag_selected_df[imag_selected_df[i] != 314.159][i]  # Use boolean indexing instead of query
+        real_serie = real_selected_df.loc[imag_serie.index,i]
+        
+        if i ==0:
+            imag_selected_no50Hz=pd.DataFrame(imag_serie).copy(deep=True).reset_index(drop=True)
+            real_selected_no50Hz=pd.DataFrame(real_serie).copy(deep=True).reset_index(drop=True)
+        else:
+            real_selected_no50Hz=real_selected_no50Hz.merge(pd.DataFrame(real_serie).reset_index(drop=True), how='outer', left_index=True, right_index=True)
+            imag_selected_no50Hz=imag_selected_no50Hz.merge(pd.DataFrame(imag_serie).reset_index(drop=True), how='outer', left_index=True, right_index=True)
+
+    if plot_figure:
+        fig=plt.figure()
+        ax=fig.add_subplot()
+        ax.set_xlabel('Real Axis',fontsize=25)
+        ax.set_ylabel('Imaginary Axis',fontsize=25)
+        ax.set_xlim(x_region)
+        ax.set_ylim(y_region)
+        ax.tick_params(labelsize=20)
+        fig.tight_layout()
+        plt.grid()
+        ax.scatter(real_selected_no50Hz,imag_selected_no50Hz, label='Eigenvalues')
+        ax.legend(loc='lower center',bbox_to_anchor=(0.45, -0.65),fontsize=15, ncol=2)
+        ax.set_title('Modal Map Zoom - No 50Hz eigs', fontsize=25)
+        plt.show()
+    return real_selected_no50Hz, imag_selected_no50Hz
+
+def check_cluster_memberships(labels_reshape,k):
+    for i in range(labels_reshape.shape[1]):
+        for l in range(0,k):
+            if len(np.where(labels_reshape[:,i]==k))==0:
+                print('missing eigenvalue in cluster '+str(k))
+        
 
 #%% import and clean data
 df_real=pd.read_csv('../results/datagen_ACOPF_LF09_seed17_nc5_ns5_d5_20241119_115327_8464/case_df_real.csv')
@@ -23,8 +67,8 @@ df_real_clean = df_real.dropna(subset=['2'])
 df_imag_clean = df_imag.dropna(subset=['2'])
 
 # remove the first column and the  case_id column
-df_real_clean = df_real_clean.drop([df_real_clean.columns[0], 'case_id', 'Stability'], axis=1)
-df_imag_clean = df_imag_clean.drop([df_imag_clean.columns[0], 'case_id', 'Stability'], axis=1)
+df_real_clean = df_real_clean.drop([df_real_clean.columns[0], 'case_id', 'Stability'], axis=1).reset_index(drop=True)
+df_imag_clean = df_imag_clean.drop([df_imag_clean.columns[0], 'case_id', 'Stability'], axis=1).reset_index(drop=True)
 
 n_cases_clean= len(df_real_clean)
 
@@ -33,49 +77,114 @@ n_cases_clean= len(df_real_clean)
 df_real_clean_max = df_real_clean[df_real_clean.columns[0:2]]
 df_imag_clean_max = df_imag_clean[df_imag_clean.columns[0:2]]
 
-
 #%% plot the modal map 
 
-# fig=plt.figure()
-# ax=fig.add_subplot()
-# ax.set_xlabel('Real Axis',fontsize=25)
-# ax.set_ylabel('Imaginary Axis',fontsize=25)
-# # ax.set_xlim([-80,20])
-# # ax.set_ylim([200,320])
-# ax.tick_params(labelsize=20)
-# fig.tight_layout()
-# plt.grid()
-# ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
-# ax.scatter(df_real_clean_max, df_imag_clean_max, label='Max Eigenvalues')
-# ax.legend(loc='lower center',bbox_to_anchor=(0.45, -0.65),fontsize=15, ncol=2)
-# ax.set_title('Modal Map', fontsize=25)
-# plt.show()
+fig=plt.figure()
+ax=fig.add_subplot()
+ax.set_xlabel('Real Axis',fontsize=25)
+ax.set_ylabel('Imaginary Axis',fontsize=25)
+# ax.set_xlim([-80,20])
+# ax.set_ylim([200,32-0])
+ax.tick_params(labelsize=20)
+fig.tight_layout()
+plt.grid()
+ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
+ax.scatter(df_real_clean_max, df_imag_clean_max, label='Max Eigenvalues')
+ax.legend(loc='lower center',bbox_to_anchor=(0.45, -0.65),fontsize=15, ncol=2)
+ax.set_title('Modal Map', fontsize=25)
+plt.show()
 
 #%% clean the data before clustering
 
 complete = 1;
 if complete==1:
-    x_region = [-80,20]
+    x_region = [-115,20]
     y_region = [200,320]
 else:
     x_region = [-1,20]
     y_region = [-400,400]
+    
+#%%
+
+fig=plt.figure()
+ax=fig.add_subplot()
+ax.set_xlabel('Real Axis',fontsize=25)
+ax.set_ylabel('Imaginary Axis',fontsize=25)
+ax.set_xlim(x_region)
+ax.set_ylim(y_region)
+ax.tick_params(labelsize=20)
+fig.tight_layout()
+plt.grid()
+ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
+ax.scatter(df_real_clean_max, df_imag_clean_max, label='Max Eigenvalues')
+ax.legend(loc='lower center',bbox_to_anchor=(0.45, -0.65),fontsize=15, ncol=2)
+ax.set_title('Modal Map Zoom', fontsize=25)
+plt.show()
+
+#%%
 
 # select eigenvalues in the region
 selected_eig_real = df_real_clean[(df_real_clean > x_region[0]) & (df_real_clean < x_region[1])]
 selected_eig_imag = df_imag_clean[(df_imag_clean > y_region[0]) & (df_imag_clean < y_region[1])]
 
-# Convert DataFrame to numpy arrays to flatten
-selected_eig_real_flat = selected_eig_real.to_numpy().flatten()
-selected_eig_imag_flat = selected_eig_imag.to_numpy().flatten()
+# create two dataframes, one for real parts and one for imaginary parts.
+# Each dataframe will have dimensions n_eig x n_cases
+real_selected_df=pd.DataFrame()
+imag_selected_df=pd.DataFrame()
 
-# Remove NaN values using a valid mask
-valid_mask = ~np.isnan(selected_eig_real_flat) & ~np.isnan(selected_eig_imag_flat)
-selected_eig_real_clean = selected_eig_real_flat[valid_mask]
-selected_eig_imag_clean = selected_eig_imag_flat[valid_mask]
+for i in range(len(selected_eig_imag)):
+    imag_selected=selected_eig_imag.loc[i,selected_eig_imag.columns].dropna()
+    ind_imag_selected=imag_selected.index
+    
+    real_selected=selected_eig_real.iloc[i][ind_imag_selected].dropna()
+    imag_selected=imag_selected[real_selected.index]
+    
+    if i==0:
+        real_selected_df=pd.DataFrame(real_selected).copy(deep=True).reset_index(drop=True)
+        imag_selected_df=pd.DataFrame(imag_selected).copy(deep=True).reset_index(drop=True)
+        
+    else:
+        real_selected_df=real_selected_df.merge(pd.DataFrame(real_selected).reset_index(drop=True), how='outer', left_index=True, right_index=True)
+        imag_selected_df=imag_selected_df.merge(pd.DataFrame(imag_selected).reset_index(drop=True), how='outer', left_index=True, right_index=True)
+
+#%%
+
+imag_selected_df=imag_selected_df.dropna()
+real_selected_df=real_selected_df.dropna()
+
+#%%
+## If you want to exclude 50 Hz eigenvalues
+
+# real_selected_no50Hz,imag_selected_no50Hz=exclude_50Hz_eig(real_selected_df, imag_selected_df)
+
+# real_selected_df=real_selected_no50Hz.copy(deep=True).dropna()
+# imag_selected_df=imag_selected_no50Hz.copy(deep=True).dropna()
+
+
+
+#%%
+
+# Convert DataFrame to numpy arrays to flatten
+selected_eig_real_flat = real_selected_df.to_numpy().flatten()
+selected_eig_imag_flat = imag_selected_df.to_numpy().flatten()
+
 
 # Combine the cleaned real and imaginary parts
-X = np.vstack([selected_eig_real_clean, selected_eig_imag_clean]).T
+X = np.vstack([selected_eig_real_flat, selected_eig_imag_flat]).T
+
+    
+#%%
+# # Convert DataFrame to numpy arrays to flatten
+# selected_eig_real_flat = selected_eig_real.to_numpy().flatten()
+# selected_eig_imag_flat = selected_eig_imag.to_numpy().flatten()
+
+# # Remove NaN values using a valid mask
+# valid_mask = ~np.isnan(selected_eig_real_flat) & ~np.isnan(selected_eig_imag_flat)
+# selected_eig_real_clean = selected_eig_real_flat[valid_mask]
+# selected_eig_imag_clean = selected_eig_imag_flat[valid_mask]
+
+# # Combine the cleaned real and imaginary parts
+# X = np.vstack([selected_eig_real_clean, selected_eig_imag_clean]).T
 
 # scale the data 
 # X_scaled = StandardScaler().fit_transform(X)
@@ -83,31 +192,38 @@ X = np.vstack([selected_eig_real_clean, selected_eig_imag_clean]).T
 #%% clustering methods
 
 #%% k means cluster
-# k = 3 # Number of clusters
-# kmeans = KMeans(n_init=10, n_clusters=k, random_state=42)
-# kmeans.fit(X)
+k = 3 # Number of clusters
+kmeans = KMeans(n_init=10, n_clusters=k, random_state=42)
+kmeans.fit(X)
 
-# labels = kmeans.labels_  # Cluster labels for each data point
-# centers = kmeans.cluster_centers_  # Coordinates of the cluster centers
+labels = kmeans.labels_  # Cluster labels for each data point
+centers = kmeans.cluster_centers_  # Coordinates of the cluster centers
+labels_reshape=labels.reshape(imag_selected_df.shape)
 
-# fig = plt.figure()
-# ax=fig.add_subplot()
-# ax.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
-# ax.set_xlabel('Real Axis',fontsize=25)
-# ax.set_ylabel('Imaginary Axis',fontsize=25)
-# ax.tick_params(labelsize=20)
-# ax.set_xlim(x_region)
-# ax.set_ylim(y_region)
-# ax.scatter(centers[:, 0], centers[:, 1], c='red', s=200, marker='x', label='Centers')  # Plot the centers
-# ax.set_title('K-Means Clustering')
-# ax.set_xlabel('Real Axis')
-# ax.set_ylabel('Imaginary Axis')
-# ax.legend()
-# ax.grid()
-# fig.tight_layout()
-# plt.show()
+fig = plt.figure()
+ax=fig.add_subplot()
+ax.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
+ax.set_xlabel('Real Axis',fontsize=25)
+ax.set_ylabel('Imaginary Axis',fontsize=25)
+ax.tick_params(labelsize=20)
+ax.set_xlim(x_region)
+ax.set_ylim(y_region)
+ax.scatter(centers[:, 0], centers[:, 1], c='red', s=200, marker='x', label='Centers')  # Plot the centers
+ax.set_title('K-Means Clustering')
+ax.set_xlabel('Real Axis')
+ax.set_ylabel('Imaginary Axis')
+ax.legend()
+ax.grid()
+fig.tight_layout()
+plt.show()
 
+check_cluster_memberships(labels_reshape, k)
 
+for l in range(k):
+    X_cl=X[labels==l]
+    if X_cl[:,0].max()>=0:
+        print('Cluster '+str(l)+' collects critical eigenvalues')
+        
 #%% SpectralClustering model
 ## note: this method doesn't work with the complete map because there is too much data 
 # n_clusters = 2  # Number of clusters you want to identify
@@ -136,27 +252,27 @@ X = np.vstack([selected_eig_real_clean, selected_eig_imag_clean]).T
 # print(f"Silhouette Score: {silhouette:.2f}")
 
 #%% DBScan
-# epsvals = [0.1, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]
-# for i in epsvals: 
-#     dbscan = DBSCAN(eps=i, min_samples=10)
-#     labels = dbscan.fit_predict(X)
+epsvals = [0.1]#, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]
+for i in epsvals: 
+    dbscan = DBSCAN(eps=i, min_samples=10)
+    labels = dbscan.fit_predict(X)
     
-#     # Plot the results
-#     fig = plt.figure()
-#     ax=fig.add_subplot()
-#     ax.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
-#     ax.set_xlabel('Real Axis',fontsize=25)
-#     ax.set_ylabel('Imaginary Axis',fontsize=25)
-#     ax.tick_params(labelsize=20)
-#     # ax.set_xlim(x_region)
-#     # ax.set_ylim(y_region)
-#     ax.set_title(f'DBSCAN Clustering with eps = {i}')
-#     ax.set_xlabel('Real Axis')
-#     ax.set_ylabel('Imaginary Axis')
-#     ax.legend()
-#     ax.grid()
-#     fig.tight_layout()
-#     plt.show()
+    # Plot the results
+    fig = plt.figure()
+    ax=fig.add_subplot()
+    ax.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
+    ax.set_xlabel('Real Axis',fontsize=25)
+    ax.set_ylabel('Imaginary Axis',fontsize=25)
+    ax.tick_params(labelsize=20)
+    # ax.set_xlim(x_region)
+    # ax.set_ylim(y_region)
+    ax.set_title(f'DBSCAN Clustering with eps = {i}')
+    ax.set_xlabel('Real Axis')
+    ax.set_ylabel('Imaginary Axis')
+    ax.legend()
+    ax.grid()
+    fig.tight_layout()
+    plt.show()
 #     fig.savefig(f'Complete_DBSCAN_eps={i}.png')
 
 # # calculate silhouette score
@@ -193,10 +309,18 @@ for i in xivals:
 
 #%% Calculate the damping index
 
+labels_reshape_T=labels_reshape.T
+
+critical_eigs=[]
+
+for i in range(labels_reshape_T.shape[0]):
+    eig_cl_index= imag_selected_df.loc[i,labels_reshape_T[i]==2]
+    
+    
 # this equation calculates the DI of all of the eigenvalues (not in groups)
-DI_tot = 1-(-X[:,0]/np.sqrt(X[:,0]**2+X[:,1]**2))
+DI_tot[i] = 1-(-np.arrayreal_parts/np.sqrt(real_parts**2+imag_parts**2))
 DI_tot_min = min(DI_tot)
-DI_tot_max = max(DI_tot)
+DI_tot_max[i] = max(DI_tot)
 
 # calculate the damping index for each group 
 DI = {}
