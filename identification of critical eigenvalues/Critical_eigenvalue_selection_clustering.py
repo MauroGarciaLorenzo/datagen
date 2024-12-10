51c417ef214c875880a727736edcd8722745b878
Heather Walker <hmacwalker1@gmail.com>
2024-12-10 12:54:06 +0100

    added parameter adjustment for K Means

--- identification of critical eigenvalues/Critical_eigenvalue_selection_clustering.py
+++ identification of critical eigenvalues/Critical_eigenvalue_selection_clustering.py
@@ -9,6 +9,8 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import OPTICS
+from sklearn.cluster import HDBSCAN
+from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.cm as cm
 from sklearn.preprocessing import StandardScaler
@@ -83,46 +85,43 @@ df_imag_clean_max = df_imag_clean[df_imag_clean.columns[0:2]]
 
 #%% plot the modal map 
 
-fig=plt.figure()
-ax=fig.add_subplot()
-ax.set_xlabel('Real Axis',fontsize=25)
-ax.set_ylabel('Imaginary Axis',fontsize=25)
+# fig=plt.figure()
+# ax=fig.add_subplot()
+# ax.set_xlabel('Real Axis',fontsize=25)
+# ax.set_ylabel('Imaginary Axis',fontsize=25)
 
-ax.tick_params(labelsize=20)
-fig.tight_layout()
-plt.grid()
-ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
-ax.scatter(df_real_clean_max, df_imag_clean_max, label='Max Eigenvalues')
-ax.legend(loc='lower center',bbox_to_anchor=(0.45, -0.65),fontsize=15, ncol=2)
-ax.set_title('Modal Map', fontsize=25)
-plt.show()
+# ax.tick_params(labelsize=20)
+# fig.tight_layout()
+# plt.grid()
+# ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
+# ax.scatter(df_real_clean_max, df_imag_clean_max, label='Max Eigenvalues')
+# ax.legend(loc='lower left', fontsize=15, ncol=1)  
+# ax.set_title('Modal Map', fontsize=25)
+# plt.show()
 
 #%% select the targeted region 
 
-complete = 1; # 1 for the complete set, 0 for the sepcific region 
-if complete==1:
+complete = 0; # 1 for the complete set, 0 for the sepcific region 
+if complete==0:
     x_region = [-115,20]
     y_region = [200,320]
-else:
-    x_region = [-1,20]
-    y_region = [-400,400]
     
 #%% Plot the Modal Map 
 
-fig=plt.figure()
-ax=fig.add_subplot()
-ax.set_xlabel('Real Axis',fontsize=25)
-ax.set_ylabel('Imaginary Axis',fontsize=25)
-ax.set_xlim(x_region)
-ax.set_ylim(y_region)
-ax.tick_params(labelsize=20)
-fig.tight_layout()
-plt.grid()
-ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
-ax.scatter(df_real_clean_max, df_imag_clean_max, label='Max Eigenvalues')
-ax.legend(loc='lower center',bbox_to_anchor=(0.45, -0.65),fontsize=15, ncol=2)
-ax.set_title('Modal Map', fontsize=25)
-plt.show()
+# fig=plt.figure()
+# ax=fig.add_subplot()
+# ax.set_xlabel('Real Axis',fontsize=25)
+# ax.set_ylabel('Imaginary Axis',fontsize=25)
+# ax.set_xlim(x_region)
+# ax.set_ylim(y_region)
+# ax.tick_params(labelsize=20)
+# fig.tight_layout(pad=4.0)
+# plt.grid()
+# ax.scatter(df_real_clean,df_imag_clean, label='Eigenvalues')
+# ax.scatter(df_real_clean_max, df_imag_clean_max, label='Max Eigenvalues')
+# ax.legend(loc='lower left', fontsize=15, ncol=1)  
+# ax.set_title('Modal Map', fontsize=25)
+# plt.show()
 
 #%% delete the nan values and their cooresponding values from selected_real and selected_imag
 
@@ -149,13 +148,11 @@ for i in range(len(selected_eig_imag)): # for each row
     else:
         real_selected_df=real_selected_df.merge(pd.DataFrame(real_selected).reset_index(drop=True), how='outer', left_index=True, right_index=True)
         imag_selected_df=imag_selected_df.merge(pd.DataFrame(imag_selected).reset_index(drop=True), how='outer', left_index=True, right_index=True)
-
 #%% 
 imag_selected_df=imag_selected_df.dropna()
 real_selected_df=real_selected_df.dropna()
 
-#%%
-## If you want to exclude 50 Hz eigenvalues
+#%% If you want to exclude 50 Hz eigenvalues
 
 # real_selected_no50Hz,imag_selected_no50Hz=exclude_50Hz_eig(real_selected_df, imag_selected_df)
 
@@ -187,68 +184,51 @@ X = np.vstack([selected_eig_real_flat, selected_eig_imag_flat]).T
 # # Combine the cleaned real and imaginary parts
 # X = np.vstack([selected_eig_real_clean, selected_eig_imag_clean]).T
 
-#%% clustering methods
 
-#%% k means cluster
-k = 3 # Number of clusters
-kmeans = KMeans(n_init=10, n_clusters=k, random_state=42)
-kmeans.fit(X)
+#%% K Means cluster
+# cluster_method = 'KMeans'
 
-joblib.dump(kmeans, 'kmeans3clusters.sav')
-
-labels = kmeans.labels_  # Cluster labels for each data point
-centers = kmeans.cluster_centers_  # Coordinates of the cluster centers
-
-# fig = plt.figure()
-# ax=fig.add_subplot()
-# ax.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
-# ax.set_xlabel('Real Axis',fontsize=25)
-# ax.set_ylabel('Imaginary Axis',fontsize=25)
-# ax.tick_params(labelsize=20)
-# ax.set_xlim(x_region)
-# ax.set_ylim(y_region)
-# ax.scatter(centers[:, 0], centers[:, 1], c='red', s=200, marker='x', label='Centers')  # Plot the centers
-# ax.set_title('K-Means Clustering')
-# ax.set_xlabel('Real Axis')
-# ax.set_ylabel('Imaginary Axis')
-# ax.legend()
-# ax.grid()
-# fig.tight_layout()
-# plt.show()
-
-
-#%% DBScan
-# epsvals = [0.1]#, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]
-# for i in epsvals: 
-#     dbscan = DBSCAN(eps=i, min_samples=10)
-#     labels = dbscan.fit_predict(X)
+# kvals = [2,3,4,5,6]
+# K_Means_Results = np.column_stack((kvals, np.zeros(len(kvals))))
+# for idx, i in enumerate(kvals):
+#     kmeans = KMeans(n_init=10, n_clusters=i, random_state=42)
+#     kmeans.fit(X)  
+#     labels = kmeans.labels_  # Cluster labels for each data point
+#     centers = kmeans.cluster_centers_  # Coordinates of the cluster centers
+#     K_Means_Results[idx,1] = silhouette_score(X, labels)
     
-#     # Plot the results
-#     fig = plt.figure()
-#     ax=fig.add_subplot()
-#     ax.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
-#     ax.set_xlabel('Real Axis',fontsize=25)
-#     ax.set_ylabel('Imaginary Axis',fontsize=25)
-#     ax.tick_params(labelsize=20)
-#     # ax.set_xlim(x_region)
-#     # ax.set_ylim(y_region)
-#     ax.set_title(f'DBSCAN Clustering with eps = {i}')
-#     ax.set_xlabel('Real Axis')
-#     ax.set_ylabel('Imaginary Axis')
-#     ax.legend()
-#     ax.grid()
-#     fig.tight_layout()
-#     plt.show()
-# #     fig.savefig(f'Complete_DBSCAN_eps={i}.png')
+# # the best silhouette score is when n_clusters = 3 
+# kmeans = KMeans(n_init=10, n_clusters=3, random_state=42)
+# kmeans.fit(X)
+# labels = kmeans.labels_  # Cluster labels for each data point
+# centers = kmeans.cluster_centers_  # Coordinates of the cluster centers
 
-# # calculate silhouette score
-# # Exclude noise points
-# cluster_labels = labels[labels != -1]  # Exclude noise
-# filtered_X = X[labels != -1]           # Exclude noise points in data
-# silhouette = silhouette_score(filtered_X, cluster_labels)
-# print(f"Silhouette Score: {silhouette:.2f}")
+# joblib.dump(kmeans, 'kmeans3clusters.sav')
+
+#%% DBSCAN
+cluster_method = 'DBSCAN'
+
+epsvals = [1, 5, 10]
+min_samples = [5, 10, 15]
+DBSCAN_results = np.zeros((len(epsvals) * len(min_samples), 3))
+
+counter = 0
+for i in epsvals:
+    for j in min_samples:
+        # Apply DBSCAN clustering
+        dbscan = DBSCAN(eps=i, min_samples=j)
+        labels = dbscan.fit_predict(X)
+        
+        # Store the eps, min_samples, and silhouette score
+        DBSCAN_results[counter, 0] = i  # eps value
+        DBSCAN_results[counter, 1] = j  # min_samples value
+        DBSCAN_results[counter, 2] = silhouette_score(X, labels)  # silhouette score
+        
+        # Increment counter
+        counter += 1
 
 #%% Optics 
+
 # xivals = [0.01, 0.05, 0.10, 0.15, 0.2]
 # for i in xivals: 
 #     optics = OPTICS(min_samples=2, xi=i, min_cluster_size=0.1)
@@ -266,73 +246,109 @@ centers = kmeans.cluster_centers_  # Coordinates of the cluster centers
 #     ax.set_title('Optics Clustering')
 #     ax.set_xlabel('Real Axis')
 #     ax.set_ylabel('Imaginary Axis')
-#     ax.legend()
+#     ax.legend(loc='lower left', fontsize=15, ncol=1)  
 #     ax.grid()
 #     fig.tight_layout()
 #     plt.show()
 #     fig.savefig(f'Complete_OPTICS_xi={i}.png')
 
-#%% 
-labels_reshape=labels.reshape(imag_selected_df.shape)
-ClustersMissingEigs, SampleClusterPairs = check_cluster_memberships(labels_reshape, k)
+#%% HDBSCAN 
 
-# if there are samples that do not have an eigenvalue in each cluster
-if ClustersMissingEigs == 1:
-    for i in range(len(SampleClusterPairs)): # for each samplecluster pair         
-        center = centers[SampleClusterPairs[i][1]] # cluster centroid 
-        
-        # values in the sample:
-        real = real_selected_df.T.iloc[SampleClusterPairs[i][0],:]
-        imag = imag_selected_df.T.iloc[SampleClusterPairs[i][0],:]
-        samplevals = pd.concat([real, imag], axis=1).reset_index(drop=True)
-        
-        min_distance = float('inf')  # Start with a very large number
-        min_index = -1  # To store the index of the minimum distance
-        
-        # for each value in sample 
-        for j in range(len(samplevals)):
-            # Calculate the Euclidean distance between the center and the sample 
-            distance = np.sqrt(np.sum((samplevals.iloc[j] - center) ** 2))
-            
-            # Check if this distance is the minimum
-            if distance < min_distance:
-                min_distance = distance
-                min_index = j
-        
-        # reassign the eigenvalue to that cluster 
-        labels_reshape[min_index,SampleClusterPairs[i][0]] = SampleClusterPairs[i][1]
+# cluster_method = 'HDBSCAN'
+# hdb = HDBSCAN()
+# hdb.fit(X)
+# labels = hdb.labels_  
 
-for l in range(k):
-    X_cl=X[labels==l]
-    if X_cl[:,0].max()>=0:
-        print('Cluster '+str(l)+' collects critical eigenvalues')
-        crit_cluster=l
+#%% plot the clusters
 
-#%% Calculate the damping ratios
-labels_reshape_T=labels_reshape.T
 unique_labels = np.unique(labels)
 
-# DI[1] has the damping index values for cluster 1, DI[2] has the values for cluster 2, etc. 
-drs = {} 
-# go through each label 
-for i in unique_labels:
-    # initialize an empty DI array that is the same size as labels_reshape_T
-    dr = np.full_like(labels_reshape_T, np.nan, dtype = float)
-    for row_idx, row in enumerate(labels_reshape_T):
-        for col_idx, value in enumerate(row):
-            if value == i: # if the label is the label we are looking for
-                dr[row_idx, col_idx] =  (-real_selected_df.T.iloc[row_idx, col_idx]/np.sqrt(real_selected_df.T.iloc[row_idx, col_idx]**2+imag_selected_df.T.iloc[row_idx, col_idx]**2))
-    drs[i] = dr
+fig = plt.figure()
+ax=fig.add_subplot()
+scatter=ax.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
+handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=scatter.cmap(scatter.norm(label)), markersize=10) for label in unique_labels]
+ax.legend(handles=handles, labels=[str(label) for label in unique_labels], loc='lower left', bbox_to_anchor=(0, 0), fontsize=15, ncol=1)
+if cluster_method == 'KMeans':
+    ax.scatter(centers[:, 0], centers[:, 1], c='red', s=200, marker='x', label='Centers')  
+ax.set_xlim(x_region)
+ax.set_ylim(y_region)
+ax.tick_params(labelsize=20)
+ax.set_xlabel('Real Axis',fontsize=25)
+ax.set_ylabel('Imaginary Axis',fontsize=25)
+ax.set_title(cluster_method + ' Clustering')
+ax.grid()
+fig.tight_layout()
+plt.show()
 
-#%% calculate the damping index 
+#%% Calculate the silhouette score 
 
-dr=drs[crit_cluster]
-DIs=np.zeros([dr.shape[0],1])
-for i in range(dr.shape[0]):
-    if np.all(np.isnan(dr[i, :])):
-        DIs[i, 0] = np.nan  # Assign NaN if no valid values exist
-    else:
-        DIs[i,0]=1-min(dr[i,~np.isnan(dr[i,:])])
+silhouette = silhouette_score(X, labels)
+print(f"Silhouette Score: {silhouette:.2f}")
+
+
+#%% check to see if there are samples that do not have an eigenvalue in each cluster
+# labels_reshape=labels.reshape(imag_selected_df.shape)
+# ClustersMissingEigs, SampleClusterPairs = check_cluster_memberships(labels_reshape, unique_labels)
+
+# # if there are samples that do not have an eigenvalue in each cluster
+# if ClustersMissingEigs == 1:
+#     for i in range(len(SampleClusterPairs)): # for each samplecluster pair         
+#         center = centers[SampleClusterPairs[i][1]] # cluster centroid 
+        
+#         # values in the sample:
+#         real = real_selected_df.T.iloc[SampleClusterPairs[i][0],:]
+#         imag = imag_selected_df.T.iloc[SampleClusterPairs[i][0],:]
+#         samplevals = pd.concat([real, imag], axis=1).reset_index(drop=True)
+        
+#         min_distance = float('inf')  # Start with a very large number
+#         min_index = -1  # To store the index of the minimum distance
+        
+#         # for each value in sample 
+#         for j in range(len(samplevals)):
+#             # Calculate the Euclidean distance between the center and the sample 
+#             distance = np.sqrt(np.sum((samplevals.iloc[j] - center) ** 2))
+            
+#             # Check if this distance is the minimum
+#             if distance < min_distance:
+#                 min_distance = distance
+#                 min_index = j
+        
+#         # reassign the eigenvalue to that cluster 
+#         labels_reshape[min_index,SampleClusterPairs[i][0]] = SampleClusterPairs[i][1]
+
+# for l in range(unique_labels):
+#     X_cl=X[labels==l]
+#     if X_cl[:,0].max()>=0:
+#         print('Cluster '+str(l)+' collects critical eigenvalues')
+#         crit_cluster=l
+        
+        
+# #%% Calculate the damping ratios
+# labels_reshape_T=labels_reshape.T
+
+# # DI[1] has the damping index values for cluster 1, DI[2] has the values for cluster 2, etc. 
+# drs = {} 
+# # go through each label 
+# for i in unique_labels:
+#     # initialize an empty DI array that is the same size as labels_reshape_T
+#     dr = np.full_like(labels_reshape_T, np.nan, dtype = float)
+#     for row_idx, row in enumerate(labels_reshape_T):
+#         for col_idx, value in enumerate(row):
+#             if value == i: # if the label is the label we are looking for
+#                 dr[row_idx, col_idx] =  (-real_selected_df.T.iloc[row_idx, col_idx]/np.sqrt(real_selected_df.T.iloc[row_idx, col_idx]**2+imag_selected_df.T.iloc[row_idx, col_idx]**2))
+#     drs[i] = dr
+        
+
+
+# #%% calculate the damping index 
+
+# dr=drs[crit_cluster]
+# DIs=np.zeros([dr.shape[0],1])
+# for i in range(dr.shape[0]):
+#     if np.all(np.isnan(dr[i, :])):
+#         DIs[i, 0] = np.nan  # Assign NaN if no valid values exist
+#     else:
+#         DIs[i,0]=1-min(dr[i,~np.isnan(dr[i,:])])
     
 
 
