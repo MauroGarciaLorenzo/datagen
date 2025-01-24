c5af5074e02c6cf877c922943360d39138e3589e
Heather Walker <hmacwalker1@gmail.com>
2025-01-24 12:36:16 +0100

    add importing of functions and model

--- identification_of_critical_eigenvalues/Calculate_DI.py
+++ identification_of_critical_eigenvalues/Calculate_DI.py
@@ -13,6 +13,11 @@ import matplotlib.cm as cm
 from sklearn.preprocessing import StandardScaler
 import copy
 import joblib
+from clustering_selection import import_and_clean
+from clustering_selection import get_clustering_region_data
+from clustering_selection import plot_clusters
+from joblib import dump, load
+from clustering_selection import best_method, x_region, y_region
 
 def check_cluster_memberships(labels_reshape, k):
     ClustersMissingEigs = 0
@@ -27,82 +32,80 @@ def check_cluster_memberships(labels_reshape, k):
     
     return ClustersMissingEigs, SampleClusterPairs
 
-
+def reassign_eigenvalues(real_selected_df, imag_selected_df, X, labels):
+    # if there are samples without an eigenvalue in each cluster, then reassign the closest eigenvalue in the sample to that cluster
+    unique_labels = np.unique(labels)
+    labels_reshape=labels.reshape(imag_selected_df.shape)
+    ClustersMissingEigs, SampleClusterPairs = check_cluster_memberships(labels_reshape, unique_labels)
+    
+    # # if there are samples that do not have an eigenvalue in each cluster
+    if ClustersMissingEigs == 1:
+        for i in range(len(SampleClusterPairs)): # for each samplecluster pair 
+            
+            # calculate the centroid of the cluster 
+            centroid = np.array([X[labels == SampleClusterPairs[i][1]].mean(axis=0)])
+            
+            # values in the sample:
+            real = real_selected_df.T.iloc[SampleClusterPairs[i][0],:]
+            imag = imag_selected_df.T.iloc[SampleClusterPairs[i][0],:]
+            samplevals = pd.concat([real, imag], axis=1).reset_index(drop=True)
+            
+            min_distance = float('inf')  # Start with a very large number
+            min_index = -1  # To store the index of the minimum distance
+            
+            # for each value in sample 
+            for j in range(len(samplevals)):
+                # Calculate the Euclidean distance between the center and the sample 
+                distance = np.sqrt(np.sum((samplevals.iloc[j].values - centroid) ** 2))
+                
+                # Check if this distance is the minimum
+                if distance < min_distance:
+                    min_distance = distance
+                    min_index = j
+                    
+            # plot the clustering region and highlight the target cluster and the target point 
+            target_cluster_points = X[labels == SampleClusterPairs[i][1]]
+            target_point = samplevals.iloc[min_index].values
+            plot_clusters(best_method, X, labels, x_region, y_region, best_method.lower(), 1, target_cluster_points, target_point,  centroid)
+            # this will save 1 figure for reassignment, not all of them
+            
+            # reassign the eigenvalue to that cluster 
+            labels_reshape[min_index,SampleClusterPairs[i][0]] = SampleClusterPairs[i][1]
+   
+    return labels_reshape 
 #%% import X, real_selected_df, imag_selected_df
 
-real_selected_df = pd.read_csv('../identification_of_critical_eigenvalues/real_selected_df.csv')
-imag_selected_df = pd.read_csv('../identification_of_critical_eigenvalues/imag_selected_df.csv')
-X= np.loadtxt('../identification_of_critical_eigenvalues/X.csv', delimiter=',')
+# get the data to run the model on
+data_name = "../results/datagen_ACOPF_LF09_seed17_nc5_ns5_d5_20241119_115327_8464"
+[df_real_clean, df_imag_clean]=import_and_clean(data_name)
 
+# same region as clustering section script 
+[real_selected_df, imag_selected_df, X] = get_clustering_region_data(x_region, y_region, df_real_clean, df_imag_clean)
 
-#%% find the best model and run it again  
+# Load the model
+model = load('best_model.joblib')
 
-Results = pd.read_csv('../identification_of_critical_eigenvalues/clustering_selection_results.csv')
+# Use the model
+labels = model.predict(X)
 
-# Get the row with the highest silhouette score
-highest_silhouette_row = Results.loc[Results['silhouette_score'].idxmax()]
-print("The best model is:\n",  highest_silhouette_row)
+# best_model = type(model).__name__
 
-best_method = highest_silhouette_row.iloc[0]
-best_parameter1 = highest_silhouette_row.iloc[1]
-best_parameter2 = highest_silhouette_row.iloc[2] 
+#%% if there are samples without an eigenvalue in each cluster, then reassign the closest eigenvalue in the sample to that cluster
+labels_reshape = reassign_eigenvalues(real_selected_df, imag_selected_df, X, labels)
 
-if best_method == "KMeans":
-    kmeans = KMeans(n_init=10, n_clusters=int(best_parameter1), random_state=42)
-    kmeans.fit(X)
-    labels = kmeans.labels_  
-elif best_method == "Optics":
-    optics = OPTICS(min_samples=int(best_parameter1))
-    optics.fit(X)
-    labels = optics.labels_
-elif best_method == "DBSCAN":
-    dbscan = DBSCAN(eps=best_parameter1, min_samples=int(best_parameter2))
-    dbscan.fit(X)
-    labels = dbscan.labels_
-    
-#%% check to see if there are samples that do not have an eigenvalue in each cluster
-unique_labels = np.unique(labels)
-labels_reshape=labels.reshape(imag_selected_df.shape)
-ClustersMissingEigs, SampleClusterPairs = check_cluster_memberships(labels_reshape, unique_labels)
-
-# # if there are samples that do not have an eigenvalue in each cluster
-if ClustersMissingEigs == 1:
-    for i in range(len(SampleClusterPairs)): # for each samplecluster pair 
-        
-        # calculate the centroid of the cluster 
-        centroid = np.array([X[labels == SampleClusterPairs[i][1]].mean(axis=0)])
-        
-        # values in the sample:
-        real = real_selected_df.T.iloc[SampleClusterPairs[i][0],:]
-        imag = imag_selected_df.T.iloc[SampleClusterPairs[i][0],:]
-        samplevals = pd.concat([real, imag], axis=1).reset_index(drop=True)
-        
-        min_distance = float('inf')  # Start with a very large number
-        min_index = -1  # To store the index of the minimum distance
-        
-        # for each value in sample 
-        for j in range(len(samplevals)):
-            # Calculate the Euclidean distance between the center and the sample 
-            distance = np.sqrt(np.sum((samplevals.iloc[j].values - centroid) ** 2))
-            
-            # Check if this distance is the minimum
-            if distance < min_distance:
-                min_distance = distance
-                min_index = j
-                
-        # plot the clustering region and highlight the target cluster and the target point 
-        target_cluster_points = X[labels == SampleClusterPairs[i][1]]
-        target_point = samplevals.iloc[min_index].values
-        
-        # reassign the eigenvalue to that cluster 
-        labels_reshape[min_index,SampleClusterPairs[i][0]] = SampleClusterPairs[i][1]
-    
-#%% Calculate the damping ratios
+#%% Calculate the damping ratios for each eigenvalue 
 labels_reshape=labels.reshape(imag_selected_df.shape)
 labels_reshape_T=labels_reshape.T
 unique_labels = np.unique(labels)
 
-drs = {} 
+# calculate the damping ratios of all the eigenvalues 
+drs_all= np.full_like(labels_reshape_T, np.nan, dtype = float)
+for row_idx, row in enumerate(labels_reshape_T):
+    for col_idx, value in enumerate(row):
+        drs_all[row_idx, col_idx] =  (-real_selected_df.T.iloc[row_idx, col_idx]/np.sqrt(real_selected_df.T.iloc[row_idx, col_idx]**2+imag_selected_df.T.iloc[row_idx, col_idx]**2))
+
+# calcualte the damping ratios for the eigenvalues in each cluster 
+drs_clustered = {}  
 # go through each label 
 for i in unique_labels:
     # initialize an empty DI array that is the same size as labels_reshape_T
@@ -111,7 +114,7 @@ for i in unique_labels:
         for col_idx, value in enumerate(row):
             if value == i: # if the label is the label we are looking for
                 dr[row_idx, col_idx] =  (-real_selected_df.T.iloc[row_idx, col_idx]/np.sqrt(real_selected_df.T.iloc[row_idx, col_idx]**2+imag_selected_df.T.iloc[row_idx, col_idx]**2))
-    drs[i] = dr
+    drs_clustered[i] = dr
        
 # identify critical eigenvalues 
 for l in unique_labels:
@@ -119,24 +122,42 @@ for l in unique_labels:
     if X_cl[:,0].max()>=0:
         print('Cluster '+str(l)+' collects critical eigenvalues')
         crit_cluster=l
+        
 
-#%% calculate the damping index 
+#%% calculate the damping indices 
 
-dr=drs[crit_cluster]
-DIs=np.zeros([dr.shape[0],1])
-for i in range(dr.shape[0]):
-    if np.all(np.isnan(dr[i, :])):
-        DIs[i, 0] = np.nan  # Assign NaN if no valid values exist
+# calculate damping indices for all eigenvalues
+DIs_all=np.zeros([drs_all.shape[0],1])
+for i in range(drs_all.shape[0]):
+    if np.all(np.isnan(drs_all[i, :])):
+        DIs_all[i, 0] = np.nan  # Assign NaN if no valid values exist
     else:
-        DIs[i,0]=1-min(dr[i,~np.isnan(dr[i,:])])
-    
+        DIs_all[i,0]=1-min(drs_all[i,~np.isnan(drs_all[i,:])])
+
+# calculate damping indices for the eigenvalues in the critical cluster 
+drs_crit=drs_clustered[crit_cluster]
+DIs_crit=np.zeros([drs_crit.shape[0],1])
+for i in range(drs_crit.shape[0]):
+    if np.all(np.isnan(drs_crit[i, :])):
+        DIs_crit[i, 0] = np.nan  # Assign NaN if no valid values exist
+    else:
+        DIs_crit[i,0]=1-min(drs_crit[i,~np.isnan(drs_crit[i,:])])
+       
     
 #%% create the csv with the inputs and add a column with the DIs  
 
-case_op=pd.read_csv('../results/datagen_ACOPF_LF09_seed17_nc5_ns5_d5_20241119_115327_8464/case_df_op.csv')
+case_op=pd.read_csv(f'{data_name}/case_df_op.csv')
 # remove the rows with na in column V1 (the cases that didn't converge)
 case_op = case_op.dropna(subset=['V1'])
-case_op['DI_crit'] = DIs
-pd.DataFrame.to_csv(case_op, 'Data_DI_Crit.csv', index=False)
+
+case_op_crit = case_op.copy()
+case_op_crit['DI_crit'] = DIs_crit
+pd.DataFrame.to_csv(case_op_crit, f'DI_crit_{data_name[-4:]}.csv', index=False)
+
+case_op_all = case_op.copy()
+case_op_all['DI_all']= DIs_all
+pd.DataFrame.to_csv(case_op_all, f'DI_crit_{data_name[-4:]}.csv', index=False)
+
+
 
 
