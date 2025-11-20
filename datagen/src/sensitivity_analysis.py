11f7a25a56faf0c68d52dbba9c8cb996a0ee1e60
francesca <francesca.rossi@upc.edu>
2025-11-20 12:28:29 +0100

    change to sensitivity analysis: df_op inputs, only feasible, only correct taus

--- datagen/src/sensitivity_analysis.py
+++ datagen/src/sensitivity_analysis.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
 
-def sensitivity(cases_df, dimensions, divs_per_cell, generator):
+def sensitivity(cases_df, df_op, dimensions, divs_per_cell, generator):
     """This Sensitivity analysis is done by gathering cases and their
     evaluated outputs, then train a Random Forest, getting the importance of
     each variable in the decision. Each variable's division count is
@@ -24,17 +24,40 @@ def sensitivity(cases_df, dimensions, divs_per_cell, generator):
     # Extract labels for dimensions that are marked as independent
     labels = [dim.label for dim in dimensions if dim.independent_dimension]
 
-    # Prepare DataFrame that will hold aggregated values per dimension
-    dims_df = pd.DataFrame()
-    for label in labels:
-            matching_columns = (
-                cases_df.filter(regex=r'^' + label + r'_*', axis=1).sum(axis=1))
-            dims_df[label] = matching_columns
-    dims_df.columns = labels
+    df_op_feas = df_op.query('Stability >=0')
+    cases_df_feas = cases_df.query('Stability >=0')
+    
+    dims_df_feas = pd.DataFrame()
+    
+    dims_df_feas['p_sg'] = df_op_feas[[col for col in df_op_feas.columns if col.startswith('P_SG')]].sum(axis=1)
+    dims_df_feas['p_cig'] = df_op_feas[[col for col in df_op_feas.columns if col.startswith('P_GFOR') or col.startswith('P_GFOL')]].sum(axis=1)
+    p_gfor = df_op_feas[[col for col in df_op_feas.columns if col.startswith('P_GFOR')]].sum(axis=1)
+    dims_df_feas['perc_g_for'] = p_gfor/dims_df_feas['p_cig']
+    
+    col_sn_gfor =[col for col in  df_op_feas.columns if col.startswith('Sn_GFOR')]
+    taus_gfor = ['tau_droop_u_gfor_'+bus.split('GFOR')[1] for bus in col_sn_gfor]
+    taus_gfor = taus_gfor + ['tau_droop_f_gfor_'+bus.split('GFOR')[1] for bus in col_sn_gfor]
+   
+    col_sn_gfol =[col for col in  df_op_feas.columns if col.startswith('Sn_GFOL')]
+    taus_gfol = ['tau_droop_u_gfol_'+bus.split('GFOL')[1] for bus in col_sn_gfol]
+    taus_gfol = taus_gfol + ['tau_droop_f_gfol_'+bus.split('GFOL')[1] for bus in col_sn_gfol]
+   
+    dims_df_feas[taus_gfor] = cases_df_feas[taus_gfor]
+    dims_df_feas[taus_gfol] = cases_df_feas[taus_gfol]
+    
+    
+    
+    # # Prepare DataFrame that will hold aggregated values per dimension
+    # dims_df = pd.DataFrame()
+    # for label in labels:
+    #         matching_columns = (
+    #             cases_df.filter(regex=r'^' + label + r'_*', axis=1).sum(axis=1))
+    #         dims_df[label] = matching_columns
+    # dims_df.columns = labels
 
     # Convert to NumPy arrays: x = features, y = target (Stability)
-    x = np.array(dims_df)
-    y = np.array(cases_df["Stability"])
+    x = np.array(dims_df_feas)
+    y = np.array(cases_df_feas["Stability"])
     y = y.astype('int')
 
     # Standardize, use reproducible seed, and fit Random Forest
