ed3f4ae534a57baea68fe86759985dffa8041bd6
Francesca Rossi <58782534+RossiFran@users.noreply.github.com>
2026-01-28 16:00:08 +0100

    Delete Fecamp Test Case - SSTool directory

--- Fecamp Test Case - SSTool/00_tool/State Space Modelling/Filters/tf_WASHOUT.m
+++
@@ -1,4 +0,0 @@
-function tf_hp = tf_WASHOUT(Tw)
-    % sTw / sTw + 1
-    tf_hp  = tf([Tw 0],[Tw 1]);
-end
--- Fecamp Test Case - SSTool/00_tool/State Space Modelling/VSC/Functions/PLL/build_pll.m
+++
@@ -1,8 +0,0 @@
-function pll = build_pll(x,u,y,kp,ki)
-    A=[0];
-    B=[1];
-    C=[-ki];
-    D=[-kp];
-
-    pll = ss(A,B,C,D,'StateName',x,'inputname',u,'outputname',y);
-end
