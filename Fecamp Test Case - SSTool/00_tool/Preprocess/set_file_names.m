1833661367d93feeded2a3cb50b338d9815a5213
FRA uni <francesca.rossi@upc.edu>
2026-07-27 10:08:49 +0200

    minor changes

--- Fecamp Test Case - SSTool/00_tool/Preprocess/set_file_names.m
+++ Fecamp Test Case - SSTool/00_tool/Preprocess/set_file_names.m
@@ -3,8 +3,8 @@
 % Create excel file names
 
 excel    = [caseName '.xlsx']; 
-data_sg  = [caseName '_data_sg.xlsx'];
-data_vsc = [caseName '_data_vsc.xlsx'];
+data_sg  = [caseName 'sg.xlsx'];
+data_vsc = [caseName 'vsc.xlsx'];
 
 % Simulink models names
 linear    =  [caseName '_LIN'];
